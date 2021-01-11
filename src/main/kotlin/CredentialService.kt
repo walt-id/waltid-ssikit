4f03b3f8d85cd6aa31e1806fa3175e31bbadae03
Philipp Potisk <philipp.potisk@gmail.com>
2021-01-11 23:43:48 +0100

    Introduced CredentialService

--- src/main/kotlin/CredentialService.kt
+++ src/main/kotlin/CredentialService.kt
@@ -1,2 +1,62 @@
+import foundation.identity.jsonld.ConfigurableDocumentLoader
+import foundation.identity.jsonld.JsonLDObject
+import foundation.identity.jsonld.JsonLDUtils
+import info.weboftrust.ldsignatures.LdProof
+import info.weboftrust.ldsignatures.crypto.provider.Ed25519Provider
+import info.weboftrust.ldsignatures.crypto.provider.impl.TinkEd25519Provider
+import info.weboftrust.ldsignatures.jsonld.LDSecurityContexts
+import info.weboftrust.ldsignatures.signer.Ed25519Signature2018LdSigner
+import info.weboftrust.ldsignatures.verifier.Ed25519Signature2018LdVerifier
+import org.json.JSONObject
+import java.net.URI
+import java.util.*
+
 object CredentialService {
+
+    val kms = KeyManagementService
+
+    init {
+        Ed25519Provider.set(TinkEd25519Provider())
+    }
+
+    fun signEd25519Signature2018(issuerDid: String, domain: String, nonce: String?, jsonCred: String): LdProof {
+
+        val jsonLdObject: JsonLDObject = JsonLDObject.fromJson(jsonCred)
+        val confLoader = LDSecurityContexts.DOCUMENT_LOADER as ConfigurableDocumentLoader
+        confLoader.isEnableHttp = true
+        confLoader.isEnableHttps = true
+        confLoader.isEnableFile = true
+        confLoader.isEnableLocalCache = false
+        jsonLdObject.documentLoader = LDSecurityContexts.DOCUMENT_LOADER
+
+        // TODO set current date
+        val created = JsonLDUtils.DATE_FORMAT.parse("2017-10-24T05:33:31Z")
+
+        val issuerKeys = kms.loadKeys(kms.getKeyId(issuerDid)!!)
+
+        // following is working in version 0.4
+        // var signer = Ed25519Signature2020LdSigner(issuerKeys!!.getPrivateAndPublicKey())
+        var signer = Ed25519Signature2018LdSigner(issuerKeys!!.getPrivateAndPublicKey())
+
+        signer.creator = URI.create(issuerDid)
+        signer.created = created
+        signer.domain = domain
+        signer.nonce = nonce
+        return signer.sign(jsonLdObject)
+    }
+
+    fun addProof(credMap: Map<String, String>, ldProof: LdProof): String {
+        val signedCredMap = HashMap<String, Any>(credMap)
+        signedCredMap.put("proof", JSONObject(ldProof.toJson()))
+        return JSONObject(signedCredMap).toString()
+    }
+
+    fun verifyEd25519Signature2018(issuerDid: String, vc: String): Boolean {
+        val jsonLdObject = JsonLDObject.fromJson(vc)
+        val issuerKeys = kms.loadKeys(kms.getKeyId(issuerDid)!!)
+        // following is working in version 0.4
+        // val verifier = Ed25519Signature2020LdVerifier(issuerKeys!!.publicKey)
+        val verifier = Ed25519Signature2018LdVerifier(issuerKeys!!.publicKey)
+        return verifier.verify(jsonLdObject)
+    }
 }
--- src/test/kotlin/CredentialServiceTest.kt
+++ src/test/kotlin/CredentialServiceTest.kt
@@ -7,6 +7,8 @@ import info.weboftrust.ldsignatures.jsonld.LDSecurityContexts
 import info.weboftrust.ldsignatures.signer.EcdsaSecp256k1Signature2019LdSigner
 import info.weboftrust.ldsignatures.signer.Ed25519Signature2018LdSigner
 import info.weboftrust.ldsignatures.verifier.EcdsaSecp256k1Signature2019LdVerifier
+import info.weboftrust.ldsignatures.verifier.Ed25519Signature2018LdVerifier
+import junit.framework.Assert.assertTrue
 import org.apache.commons.codec.binary.Hex
 import org.bitcoinj.core.ECKey
 import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPrivateKey
@@ -17,6 +19,7 @@ import org.junit.Test
 import java.net.URI
 import java.security.Security
 import java.util.HashMap
+import kotlin.test.assertNotNull
 
 class CredentialServiceTest {
 
@@ -24,6 +27,37 @@ class CredentialServiceTest {
     @Before
     fun setup() {
         Security.addProvider(BouncyCastleProvider())
+    }
+
+    fun verifyEcdsaSecp256k1Credential(vc: String): Boolean {
+        val jsonLdObject = JsonLDObject.fromJson(vc)
+        // TODO Load from KMS
+        val verifier = EcdsaSecp256k1Signature2019LdVerifier(loadPublicKey("dummyIssuer"))
+        return verifier.verify(jsonLdObject)
+    }
+
+    @Test
+    fun issueEd25519CredentialTest() {
+        val kms = KeyManagementService
+        val ds = DidService
+        val cs = CredentialService
+
+        val issuerDid = ds.registerDid()
+        val domain = "example.com"
+        val nonce: String? = null
+        val credMap: Map<String, String> = mapOf("one" to "two")
+        val cred = JSONObject(credMap).toString()
+
+        val proof = cs.signEd25519Signature2018(issuerDid, domain, nonce, cred)
+        assertNotNull(proof)
+
+        val vc = cs.addProof(credMap, proof)
+        assertNotNull(vc)
+
+        println("Credential generated: ${vc}")
+
+        val ret = cs.verifyEd25519Signature2018(issuerDid, vc)
+        assertTrue(ret)
     }
 
 
@@ -58,10 +92,8 @@ class CredentialServiceTest {
 
         println(key.parameters.curve)
 
-
         // FIX: following is not working, as the key is in encoded form. we would need the raw bytes
         val ecKey = ECKey.fromASN1(privateKeyBytes)
-
 
 
         val signer = EcdsaSecp256k1Signature2019LdSigner(ecKey)
@@ -79,20 +111,12 @@ class CredentialServiceTest {
 
         println("Credential generated: ${vc}")
 
-        verifyEcdsaSecp256k1Credential(vc)
+        val ret = verifyEcdsaSecp256k1Credential(vc)
 
-    }
-
-
-    fun verifyEcdsaSecp256k1Credential(vc: String): Boolean {
-        val jsonLdObject = JsonLDObject.fromJson(vc)
-        // TODO Load from KMS
-        val verifier = EcdsaSecp256k1Signature2019LdVerifier(loadPublicKey("dummyIssuer"))
-        return verifier.verify(jsonLdObject)
     }
 
     @Test
-    fun issuerEd25519CredentialTest() {
+    fun dummyIssueEd25519CredentialTest() {
 
         val kms = KeyManagementService
         val ds = DidService
@@ -112,6 +136,8 @@ class CredentialServiceTest {
         val issuerKeys = kms.loadKeys(kms.getKeyId(issuerDid)!!)
 
         var signer = Ed25519Signature2018LdSigner(issuerKeys!!.getPrivateAndPublicKey())
+        // following is working in version 0.4
+        //var signer = Ed25519Signature2020LdSigner(issuerKeys!!.getPrivateAndPublicKey())
 
         signer.creator = creator
         signer.created = created
@@ -128,14 +154,18 @@ class CredentialServiceTest {
         val signedCredMap = HashMap<String, Any>(cred)
         signedCredMap.put("proof", JSONObject(ldProof.toJson()))
         val vc: String = JSONObject(signedCredMap).toString()
+        println(vc)
+        val jsonLdObject2 = JsonLDObject.fromJson(vc)
+        //TODO extract issuer
+        val verifier = Ed25519Signature2018LdVerifier(issuerKeys!!.publicKey)
+        assertTrue(verifier.verify(jsonLdObject2))
     }
-
 
 
     /******** following should not be needed any more *******************/
 
     private val testSecp256k1PrivateKeyString = "2ff4e6b73bc4c4c185c68b2c378f6b233978a88d3c8ed03df536f707f084e24e"
-    private val testSecp256k1PublicKeyString =  "0343f9455cd248e24c262b1341bbe37cea360e1c5ce526e5d1a71373ba6e557018"
+    private val testSecp256k1PublicKeyString = "0343f9455cd248e24c262b1341bbe37cea360e1c5ce526e5d1a71373ba6e557018"
 
     fun loadPrivateKey(issuer: String): ECKey =
         ECKey.fromPrivate(Hex.decodeHex(testSecp256k1PrivateKeyString.toCharArray()))
@@ -152,7 +182,7 @@ class CredentialServiceTest {
     }
 
     @Test
-    fun issueDummy() {
+    fun dummyIssueSecp256k1Issue() {
         val cred: Map<String, String> = mapOf("one" to "two")
 
         val jsonLdObject: JsonLDObject = JsonLDObject.fromJson(JSONObject(cred).toString())
