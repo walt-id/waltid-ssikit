21945be9f5f1720233017d7d0da76429b71dc98d
Philipp Potisk <philipp.potisk@gmail.com>
2021-01-29 12:02:42 +0100

    Draft implementation of DID key

--- src/main/kotlin/DidService.kt
+++ src/main/kotlin/DidService.kt
@@ -1,12 +1,59 @@
+import com.fasterxml.jackson.annotation.*
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.ipfs.multibase.Multibase
 import java.util.*
+
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+data class Service(
+    val id: String,
+    val type: String,
+    val serviceEndpoint: String
+)
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+data class PublicKey(
+    val id: String,
+    val controller: String,
+    val type: String,
+    val publicKeyBase58: String
+)
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+data class Did(
+    @JsonProperty("@context") val context: String,
+    val id: String,
+//    val controller: List<PublicKey>,
+    val publicKey: List<PublicKey>? = null,
+    val verificationMethod: List<Map<String, Any>>? = null,
+//    val authentication: List<String>?,
+//    val assertionMethod: List<String>?,
+//    val capabilityDelegation: List<String>?
+) {
+
+    @JsonIgnore
+    var dynFields: LinkedHashMap<String, Any> = LinkedHashMap<String, Any>()
+
+    @JsonAnySetter
+    fun setField(key: String, value: Any) {
+        dynFields[key] = value
+    }
+
+    @JsonAnyGetter
+    fun any():  LinkedHashMap<String, Any> {
+        return dynFields
+    }
+}
 
 object DidService {
 
     var kms = KeyManagementService
 
+    //
     fun registerDid(): String {
         val keyId = kms.generateEd25519KeyPair()
-        var identifier = "did:key:" + kms.getMultiBase58PublicKey(keyId)
+        // should be fingerprintFromPublicKey
+        var identifier = "did:key:z" + kms.getMultiBase58PublicKey(keyId)
 
         kms.addAlias(keyId, identifier)
 
@@ -30,5 +77,115 @@ object DidService {
         kms.addAlias(keyId, identifier)
 
         return identifier
+    }
+
+
+//    async keyToDidDoc(edKey) {
+//        const did = `did:key:${edKey.fingerprint()}`;
+//        const keyId = `${did}#${edKey.fingerprint()}`;
+//        edKey.controller = did;
+//
+//        const dhKey = await X25519KeyPair.fromEdKeyPair(edKey);
+//        dhKey.id = `${did}#${dhKey.fingerprint()}`;
+//
+//        const didDoc = {
+//            '@context': ['https://w3id.org/did/v0.11'],
+//            id: did,
+//            publicKey: [{
+//                id: keyId,
+//                type: edKey.type,
+//                controller: did,
+//                publicKeyBase58: edKey.publicKeyBase58
+//        }],
+//            authentication: [keyId],
+//            assertionMethod: [keyId],
+//            capabilityDelegation: [keyId],
+//            capabilityInvocation: [keyId],
+//            keyAgreement: [{
+//                id: dhKey.id,
+//                type: dhKey.type,
+//                controller: did,
+//                publicKeyBase58: dhKey.publicKeyBase58
+//        }]
+//        };
+//        Object.defineProperty(didDoc, 'keys', {
+//            value: {
+//            [keyId]: edKey,
+//            [dhKey.id]: dhKey
+//        },
+//            enumerable: false
+//        });
+
+
+
+    // https://w3c-ccg.github.io/did-method-key/
+    // https://github.com/digitalbazaar/did-method-key-js#example-did-document
+    fun resolveDidKey(identifier: String): String? {
+        val fingerprint = identifier.substringAfter("did:key:")
+        // fetch b58 key out of fingerprintFromPublicKey
+        val pubKeyBase58 =  Multibase.encode(Multibase.Base.Base64,fingerprint.toByteArray())
+        println(pubKeyBase58)
+        val mapper = jacksonObjectMapper()
+        val map = mapOf(
+            "@context" to "https://w3id.org/did/v1",
+            "id" to identifier,
+            "publicKey" to listOf(
+                mapOf(
+                    "id" to "did:key:${fingerprint}#${fingerprint}",
+                    "type" to "Ed25519VerificationKey2018",
+                    "controller" to identifier,
+                    "publicKeyBase58" to "B12NYF8RrR3h41TDCTJojY59usg3mbtbjnFs7Eud1Y6u" // fetch b58 key out of fingerprintFromPublicKey
+                )
+            ),
+            "authentication" to listOf("did:key:${fingerprint}#${fingerprint}"),
+            "assertionMethod" to listOf("did:key:${fingerprint}#${fingerprint}"),
+            "capabilityDelegation" to listOf("did:key:${fingerprint}#${fingerprint}"),
+            "capabilityInvocation" to listOf("did:key:${fingerprint}#${fingerprint}"),
+            "keyAgreement" to listOf(
+                mapOf(
+                    // TODO keyAgreement key -> controller + https://github.com/digitalbazaar/x25519-key-agreement-key-2019/blob/2bf26987623c16776085de137b7daef3e3e73b15/lib/X25519KeyPair.js#L235
+                    "id" to "did:key:${fingerprint}#zBzoR5sqFgi6q3iFia8JPNfENCpi7RNSTKF7XNXX96SBY4",
+                    "type" to "X25519KeyAgreementKey2019",
+                    "controller" to identifier,
+                    "publicKeyBase58" to "JhNWeSVLMYccCk7iopQW4guaSJTojqpMEELgSLhKwRr"
+                )
+            ),
+        )
+
+        val didSerialized = mapper.writeValueAsString(map)
+        println(didSerialized)
+        return didSerialized
+    }
+
+    fun createDidKey() {
+        val keyId = kms.generateEd25519KeyPair()
+        var identifier = "did:key:" + kms.getMultiBase58PublicKey(keyId)
+        kms.addAlias(keyId, identifier)
+    }
+
+    fun createDidWeb(): Map<String, Any> {
+//        async createIdentifier(
+//                { kms, alias }: { kms?: string; alias?: string },
+//        context: IContext,
+//        ): Promise<Omit<IIdentifier, 'provider'>> {
+//            const key = await context.agent.keyManagerCreate({ kms: kms || this.defaultKms, type: 'Secp256k1' })
+//
+//            const identifier: Omit<IIdentifier, 'provider'> = {
+//            did: 'did:web:' + alias,
+//            controllerKeyId: key.kid,
+//            keys: [key],
+//            services: [],
+//        }
+//            debug('Created', identifier.did)
+//            return identifier
+//        }
+
+        return mapOf(
+            "active" to true,
+            "scope" to "read write email",
+            "client_id" to "SlAV32hkKG",
+            "username" to "phil",
+            "exp" to 1437275311,
+        )
     }
 }
--- src/test/kotlin/DidServiceTest.kt
+++ src/test/kotlin/DidServiceTest.kt
@@ -1,15 +1,29 @@
-
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.junit.Before
 import org.junit.Test
 import java.security.Security
 import kotlin.test.*
 import io.ipfs.multibase.Multibase
+import java.io.File
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
+import com.fasterxml.jackson.annotation.JsonAnySetter
 
+import java.util.LinkedHashMap
+import DidServiceTest.Product
+import java.util.Collections
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter
+import com.fasterxml.jackson.annotation.JsonIgnore
 
 
 class DidServiceTest {
+
+    protected val RESOURCES_PATH: String = "src/test/resources"
+
+    fun readExampleDid(fileName: String) =
+        File("$RESOURCES_PATH/dids/${fileName}.json").readText(Charsets.UTF_8)
 
     @Before
     fun setup() {
@@ -28,7 +42,6 @@ class DidServiceTest {
 
     @Test
     fun registerDidTest() {
-
         val ds = DidService
         val identifier = ds.registerDid()
         assertNotNull(identifier)
@@ -36,4 +49,94 @@ class DidServiceTest {
         assertEquals("did:key", identifier.substring(0, 7))
         print(identifier)
     }
+
+    @Test
+    fun didKeyTest() {
+        val ds = DidService
+        val didResolved = ds.resolveDidKey("did:key:z6MkpTHR8VNsBxYAAWHut2Geadd9jSwuBV8xRoAnwWsdvktH")
+        val exampleDid = readExampleDid("did-key-example1").replace("\n", "").replace("\r", "").replace(" ", "");
+        println("------------------")
+        println(exampleDid)
+        assertEquals(exampleDid, didResolved)
+    }
+
+
+    // https://stackoverflow.com/questions/57178093/how-to-deserialize-json-with-dynamic-object
+    // https://stackoverflow.com/questions/12134231/jackson-dynamic-property-names
+    internal class Product() {
+        @JsonIgnore
+        var dynFields: LinkedHashMap<String, Any> = LinkedHashMap<String, Any>()
+
+        @JsonAnySetter
+        fun setDetail(key: String, value: Any) {
+            dynFields[key] = value
+        }
+
+        @JsonAnyGetter
+        fun any():  LinkedHashMap<String, Any> {
+            return dynFields
+        }
+    }
+
+    @Test
+    fun jsonDynObjectTest() {
+        val mapper = jacksonObjectMapper()
+        val json = readExampleDid("product")
+        val product: Product = mapper.readValue(json, Product::class.java)
+        println(product)
+        val serialized = mapper.writeValueAsString(product)
+        print(serialized)
+    }
+
+    @Test
+    fun whenSerializeMap_thenSuccess() {
+        val mapper = jacksonObjectMapper()
+        val map = mapOf(1 to "one", 2 to "two")
+
+        val serialized = mapper.writeValueAsString(map)
+
+        val json = """{"1":"one","2":"two"}"""
+        assertEquals(serialized, json)
+    }
+
+    @Test
+    fun jsonTest() {
+        val mapper = jacksonObjectMapper()
+
+//        val state = Did(
+//            "https://w3id.org/did/v1",
+//            "did:key:z6MkpTHR8VNsBxYAAWHut2Geadd9jSwuBV8xRoAnwWsdvktH",
+//            listOf(
+//                PublicKey("id1", "contr1", "type1", "key1"),
+//                PublicKey("id2", "contr2", "type2", "key2")
+//            ),
+//            null,
+//            null,
+//            null,
+//            null
+//        )
+        val did = Did(
+            "https://w3id.org/did/v1",
+            "did:key:z6MkpTHR8VNsBxYAAWHut2Geadd9jSwuBV8xRoAnwWsdvktH"
+        )
+        val writer = mapper.writer(DefaultPrettyPrinter())
+        val json = writer.writeValueAsString(did)
+        println(json)
+        val out = mapper.readValue<Did>(json)
+        println(out)
+    }
+
+    @Test
+    fun parseDids() {
+        val mapper = jacksonObjectMapper()
+        var json = readExampleDid("did-test")
+        val did = mapper.readValue<Did>(json)
+        println(did)
+        println(did.context)
+        println(did.id)
+        println(did.verificationMethod)
+        println(did.publicKey?.get(0)?.id)
+        println(did.dynFields.get("controller"))
+    }
+
 }
