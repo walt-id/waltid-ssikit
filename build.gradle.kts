f900ec1dc76e647ae37b31eb2a0954ab9a2be489
Philipp Potisk <philipp.potisk@gmail.com>
2021-04-03 15:54:16 +0200

    Fixed log4j-config

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,11 +46,11 @@ dependencies {
     implementation("io.ktor:ktor-client-cio:1.5.2")
 
     // Logging
-    implementation("org.slf4j:slf4j-api:1.8.0-beta4")
+    implementation("org.slf4j:slf4j-api:2.0.0-alpha1")
 
     implementation("org.apache.logging.log4j:log4j-core:2.14.1")
     implementation("org.apache.logging.log4j:log4j-api:2.14.1")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
+    implementation("org.apache.logging.log4j:log4j-slf4j18-impl:2.14.1")
     implementation("io.github.microutils:kotlin-logging-jvm:2.0.6")
 
     // Config
