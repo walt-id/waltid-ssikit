740b8af90b8c87b1015cda490b2cf7f7233de011
Philipp Potisk <philipp.potisk@gmail.com>
2021-06-30 13:18:37 +0200

    Created test-case for vc issuance flow

--- src/test/kotlin/org/letstrust/essif/VcIssuanceFlowTest.kt
+++ src/test/kotlin/org/letstrust/essif/VcIssuanceFlowTest.kt
@@ -1,11 +1,25 @@
 package org.letstrust.essif
 
 import org.junit.Test
+import org.letstrust.common.readEssif
+import org.letstrust.services.essif.EnterpriseWalletService
 import org.letstrust.services.essif.EosService
 import org.letstrust.services.essif.EssifFlowRunner
 import org.letstrust.services.essif.UserWalletService
 
 class VcIssuanceFlowTest {
+
+    @Test
+    fun generateDidAuthRequest() {
+        println(EnterpriseWalletService.generateDidAuthRequest())
+    }
+
+    @Test
+    fun validateDidAuthRequest() {
+        val didAuthReq = readEssif("onboarding-did-ownership-req")
+
+        println(didAuthReq)
+    }
 
     @Test
     fun testVcIssuanceFlow() {
