5ab0124282df3ad4b52a0ea3b5933581bbf68b58
Philipp Potisk <philipp.potisk@gmail.com>
2021-07-15 14:31:37 +0200

    Split Essif client/server

--- src/main/kotlin/org/letstrust/services/essif/EssifClient.kt
+++ src/main/kotlin/org/letstrust/services/essif/EssifClient.kt
@@ -1,16 +1,11 @@
 package org.letstrust.services.essif
 
-import com.nimbusds.jose.JWSAlgorithm
 import com.nimbusds.jose.crypto.X25519Decrypter
 import com.nimbusds.jose.jwk.Curve
-import com.nimbusds.jose.jwk.ECKey
-import com.nimbusds.jose.jwk.KeyUse
 import com.nimbusds.jose.jwk.OctetKeyPair
 import com.nimbusds.jose.jwk.gen.OctetKeyPairGenerator
-import com.nimbusds.jose.shaded.json.JSONObject
 import com.nimbusds.jwt.EncryptedJWT
 import com.nimbusds.jwt.SignedJWT
-import com.sksamuel.hoplite.fp.valid
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -21,29 +16,24 @@ import org.letstrust.common.OidcUtil
 import org.letstrust.crypto.KeyAlgorithm
 import org.letstrust.crypto.canonicalize
 import org.letstrust.crypto.encBase64Str
-import org.letstrust.crypto.newKeyId
 import org.letstrust.model.*
 import org.letstrust.services.did.DidService
 import org.letstrust.services.jwt.JwtService
 import org.letstrust.services.key.KeyService
 import org.letstrust.services.vc.CredentialService
-import java.security.KeyPairGenerator
-import java.security.SecureRandom
-import java.security.interfaces.ECPublicKey
-import java.security.spec.ECGenParameterSpec
 import java.util.*
 
-object EssifWalletClient {
+object EssifEosClient {
     fun generateAuthenticationRequest(): String {
-        return EssifWallet.generateAuthenticationRequest()
+        return EssifServer.generateAuthenticationRequest()
     }
 
     fun openSession(authResp: String): String {
-        return EssifWallet.openSession(authResp)
+        return EssifServer.openSession(authResp)
     }
 }
 
-object EssifWallet {
+object EssifClient {
 
     private val log = KotlinLogging.logger {}
 
@@ -71,9 +61,11 @@ object EssifWallet {
 
     // X25519
     // TODO move to KeyStore
-    val ephPrivKey: OctetKeyPair = OctetKeyPairGenerator(Curve.X25519)
+    val ephPrivKey: OctetKeyPair by lazy {
+        OctetKeyPairGenerator(Curve.X25519)
         .keyID(JwtService.keyId)
         .generate()
+    }
 
     /**
      * Runs DID + VC Auth (optional)
@@ -83,10 +75,10 @@ object EssifWallet {
 
         log.debug { "CLIENT::authenticate()" }
 
-        val authReq = EssifWalletClient.generateAuthenticationRequest()
+        val authReq = EssifEosClient.generateAuthenticationRequest()
         val didAuthReq = validateAuthenticationRequest(authReq)
         val authResp = this.generateAuthenticationResponse(didAuthReq)
-        val encAccessToken = EssifWalletClient.openSession(authResp)
+        val encAccessToken = EssifEosClient.openSession(authResp)
         val accessToken = decryptAccessToken(encAccessToken)
 
         log.debug { "Received access token for fetching credential: $accessToken" }
@@ -148,89 +140,4 @@ object EssifWallet {
 
         return encBase64Str(vpCan)
     }
-
-
-    ///////////////////////////////////////////////////////////////////////////
-    // Server
-    ///////////////////////////////////////////////////////////////////////////
-
-    // TODO: Add configuration + keystore integration
-    val kidServer = "22df3f6e54494c12bfb559e171cfe747"
-    val didServer = "did:ebsi:0x416e6e6162656c2e4c65652e452d412d506f652e"
-    val redirectUri = "http://localhost:8080/redirect"
-    val callback = "http://localhost:8080/callback"
-    val nonce = UUID.randomUUID().toString()
-
-    /**
-     * Generates OIDC-based authentication request
-     */
-    fun generateAuthenticationRequest(): String {
-
-        log.debug { "SERVER::generateAuthenticationRequest()" }
-
-        // TODO ingest correct parameters and claims
-
-        val oidcRequest = OidcUtil.generateOidcAuthenticationRequest(kidServer, didServer, redirectUri, callback, nonce)
-
-        return Json.encodeToString(oidcRequest)
-    }
-
-    /**
-     * Takes the Authentication Request, verifies it and establishes a mutual-authenticated sessions.
-     */
-    fun openSession(authResp: String): String {
-
-        log.debug { "SERVER::openSession()" }
-
-        val authRespPayload = this.validateAuthenticationResponse(authResp)
-        val encryptionKeyJsonStr = Json.encodeToString(authRespPayload.claims.encryption_key)
-        return this.generateEncryptedAccessToken(encryptionKeyJsonStr)
-    }
-
-    private fun validateAuthenticationResponse(authResp: String): AuthenticationResponsePayload {
-
-        log.debug { "SERVER::validateAuthenticationResponse()" }
-
-        // TODO validate JWT & DID & VP
-
-        val jwt = SignedJWT.parse(authResp)
-
-        println(jwt.payload)
-
-        val arp = Json.decodeFromString<AuthenticationResponsePayload>(jwt.payload.toString())
-
-        return arp
-
-//        val claims = jwt.jwtClaimsSet.getClaim("claims") as JSONObject
-//
-//        val vp = claims.get("verified_claims")
-//        val encryptionKey = claims.get("encryption_key") as JSONObject
-//
-//        println(vp)
-//
-//        return encryptionKey.toString()
-    }
-
-    private fun generateEncryptedAccessToken(encryptionKey: String): String {
-
-        log.debug { "SERVER::generateEncryptedAccessToken()" }
-
-        // Generate Access token
-        val accessToken = UUID.randomUUID().toString()
-
-        // Encrypt JWE
-        println(encryptionKey)
-        val emphClientKey = OctetKeyPair.parse(encryptionKey) // ECKey.parse(encryption_key)
-
-        val privateKeyId = KeyService.generate(KeyAlgorithm.EdDSA_Ed25519).id
-        println(privateKeyId)
-        val encToken = JwtService.encrypt(privateKeyId, emphClientKey, accessToken)
-
-        return encToken
-    }
-}
-
-fun main() {
-    LetsTrustServices.setLogLevel(Level.DEBUG)
-    EssifWallet.authenticate()
 }
