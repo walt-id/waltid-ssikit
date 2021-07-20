491b7d58f325320267e4538e5b98c02ef71980c9
Philipp Potisk <philipp.potisk@gmail.com>
2021-07-20 21:02:11 +0200

    Added missing config

--- src/test/kotlin/org/letstrust/common/OidcUtilTest.kt
+++ src/test/kotlin/org/letstrust/common/OidcUtilTest.kt
@@ -1,12 +1,22 @@
 package org.letstrust.common
 
+import id.walt.servicematrix.ServiceMatrix
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.junit.Before
 import org.junit.Test
 import org.letstrust.model.DidMethod
 import org.letstrust.services.did.DidService
+import java.security.Security
 import java.util.*
 import kotlin.test.assertEquals
 
 class OidcUtilTest {
+
+    @Before
+    fun setup() {
+        ServiceMatrix("service-matrix.properties")
+    }
+
 
     @Test
     fun authenticationRequestTest() {
