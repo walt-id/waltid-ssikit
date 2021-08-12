4df3bcf5f49650ddd7bcaa66adb860cd63943593
Kevin Burgmann <kevin@walt.id>
2021-08-12 19:12:43 +0200

    Updated high level API interface for Custodian #23

--- src/main/kotlin/id/walt/custodian/CustodianService.kt
+++ src/main/kotlin/id/walt/custodian/CustodianService.kt
@@ -1,11 +1,17 @@
 package id.walt.custodian
 
-
+import id.walt.crypto.Key
+import id.walt.crypto.KeyId
+import id.walt.vclib.model.VerifiableCredential
 
 interface CustodianService {
-    fun storeCredential()
-    fun loadCredential()
-    fun createPresentation()
-    fun importKey()
-    fun exportKey()
+    fun getKey(keyId: KeyId): Key
+    fun listKeys(): List<Key>
+    fun storeKey(key: Key)
+
+    fun getCredential(id: String): VerifiableCredential
+    fun listCredentials(): List<VerifiableCredential>
+    fun storeCredential(vc: VerifiableCredential)
+
+    // fun createPresentation()
 }
