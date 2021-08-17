d8904ec9d755d4bee94f919a1815b4736cdd3b17
Kevin Burgmann <kevin@walt.id>
2021-08-17 21:12:20 +0200

    Refactored key stores, included key store functions into custodian

--- src/main/kotlin/id/walt/custodian/CustodianService.kt
+++ src/main/kotlin/id/walt/custodian/CustodianService.kt
@@ -1,17 +1,39 @@
 package id.walt.custodian
 
 import id.walt.crypto.Key
-import id.walt.crypto.KeyId
+import id.walt.servicematrix.ServiceProvider
+import id.walt.services.WaltIdService
+import id.walt.services.keystore.KeyStoreService
 import id.walt.vclib.model.VerifiableCredential
 
-interface CustodianService {
-    fun getKey(keyId: KeyId): Key
-    fun listKeys(): List<Key>
-    fun storeKey(key: Key)
+abstract class CustodianService : WaltIdService() {
+    override val implementation get() = serviceImplementation<CustodianService>()
 
-    fun getCredential(id: String): VerifiableCredential
-    fun listCredentials(): List<VerifiableCredential>
-    fun storeCredential(vc: VerifiableCredential)
+    open fun getKey(alias: String): Key = implementation.getKey(alias)
+    open fun listKeys(): List<Key> = implementation.listKeys()
+    open fun storeKey(key: Key): Unit = implementation.storeKey(key)
+
+    open fun getCredential(id: String): VerifiableCredential = implementation.getCredential(id)
+    open fun listCredentials(): List<VerifiableCredential> = implementation.listCredentials()
+    open fun storeCredential(vc: VerifiableCredential): Unit = implementation.storeCredential(vc)
 
     // fun createPresentation()
+
+    companion object : ServiceProvider {
+        override fun getService() = object : CustodianService() {}
+    }
 }
+
+open class WaltCustodianService : CustodianService() {
+
+    private val keystore = KeyStoreService.getService()
+
+    override fun getKey(alias: String): Key = keystore.load(alias)
+    override fun listKeys(): List<Key> = keystore.listKeys()
+    override fun storeKey(key: Key) = keystore.store(key)
+
+    override fun getCredential(id: String): VerifiableCredential {
+        return super.getCredential(id)
+    }
+}
+
