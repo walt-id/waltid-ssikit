4fabd8eda40b7b56e027b7d1b6334b74b722f03c
Kevin Burgmann <kevin@walt.id>
2021-08-18 11:47:05 +0200

    Fixed auditor with VcLib and added test case to auditor (pair with sevi)

--- src/main/kotlin/id/walt/auditor/AuditorService.kt
+++ src/main/kotlin/id/walt/auditor/AuditorService.kt
@@ -1,6 +1,5 @@
 package id.walt.auditor
 
-import id.walt.servicematrix.ServiceMatrix
 import id.walt.services.vc.JsonLdCredentialService
 import id.walt.vclib.Helpers.encode
 import id.walt.vclib.VcLibManager
@@ -27,7 +26,10 @@ interface VerificationPolicy {
 class SignaturePolicy : VerificationPolicy {
     private val jsonLdCredentialService = JsonLdCredentialService.getService()
 
-    override fun verify(vp: VerifiablePresentation) = jsonLdCredentialService.verifyVp(vp.encode())
+
+    override fun verify(vp: VerifiablePresentation): Boolean {
+        return jsonLdCredentialService.verify(vp.encode()).verified
+    }
 }
 
 class JsonSchemaPolicy : VerificationPolicy { // Schema already validated by json-ld?
@@ -52,7 +54,10 @@ object PolicyRegistry {
 data class VerificationResult(
     val overallStatus: Boolean = false,
     val policyResults: Map<VerificationPolicy, Boolean>
-)
+) {
+    override fun toString() =
+        "VerificationResult(overallStatus=$overallStatus, policyResults={${policyResults.entries.joinToString { it.key.javaClass.simpleName + "=" + it.value }}})"
+}
 
 interface IAuditor {
 
@@ -76,119 +81,11 @@ object AuditorService : IAuditor {
 
         return VerificationResult(allAccepted(policyResults), policyResults)
     }
-}
 
-fun main() {
-    ServiceMatrix("service-matrix.properties")
-
-    PolicyRegistry.register(SignaturePolicy())
-    PolicyRegistry.register(TrustedIssuerDidPolicy())
-    PolicyRegistry.register(TrustedSubjectDidPolicy())
-    PolicyRegistry.register(JsonSchemaPolicy())
-
-    val res = AuditorService.verify(
-        """
-      {
-  "@context" : [ "https://www.w3.org/2018/credentials/v1" ],
-  "id": "0",
-  "type" : [ "VerifiableCredential", "VerifiablePresentation" ],
-  "verifiableCredential" : [ {
-    "@context" : [ "https://www.w3.org/2018/credentials/v1" ],
-    "id" : "1626680615978",
-    "type" : [ "VerifiableCredential", "VerifiableAttestation", "Europass" ],
-    "issuer" : "did:ebsi:22RgRjvk6mVkUaZohDtDSweQFfMXTh1YBnCZu3JgPNEmx7zZ",
-    "issuanceDate" : "2021-07-19T09:43:36.214321688",
-    "credentialSubject" : {
-      "@id" : "did:key:z6MkuHT1Q4FTSC9tqaU3bt7dCGesYNzjNMfYX1YUvWKXLpod",
-      "currentFamilyName" : "Skywalker",
-      "currentGivenName" : "Lea",
-      "dateOfBirth" : "2021-02-15",
-      "mailBox" : {
-        "@uri" : "mailto:leaskywalker@gmail.com"
-      }
-    },
-    "learningAchievement" : {
-      "@id" : "urn:epass:learningAchievement:1",
-      "title" : {
-        "text" : {
-          "@content-type" : "text/plain",
-          "@lang" : "en",
-          "#text" : "Degree in Biology"
-        }
-      },
-      "specifiedBy" : {
-        "@idref" : "urn:epass:qualification:1"
-      }
-    },
-    "learningSpecificationReferences" : {
-      "qualification" : {
-        "@id" : "urn:epass:qualification:1",
-        "title" : {
-          "text" : {
-            "@content-type" : "text/plain",
-            "@lang" : "en",
-            "#text" : "Degree in Biology"
-          }
-        },
-        "awardingOpportunities" : null,
-        "eqfLevel" : {
-          "@targetFrameworkUrl" : "http://data.europa.eu/snb/eqf/25831c2",
-          "@targetNotation" : "eqf",
-          "@uri" : "http://data.europa.eu/snb/eqf/7",
-          "targetName" : {
-            "text" : {
-              "@content-type" : "text/plain",
-              "@lang" : "en",
-              "#text" : "Level 7"
-            }
-          }
-        }
-      }
-    },
-    "targetDescription" : {
-      "text" : {
-        "@content-type" : "text/plain",
-        "@lang" : "en",
-        "#text" : "Highly specialised knowledge, some of which is at the forefront of knowledge in a field of work or study, as the basis for original thinking and/or research <html:br></html:br>critical awareness of knowledge issues in a field and at the interface between different fields </html:div> <html:div xmlns:html=\"http://www.w3.org/1999/xhtml\" class=\"Skill\">specialised problem-solving skills required in research and/or innovation in order to develop new knowledge and procedures and to integrate knowledge from different fields</html:div> <html:div xmlns:html=\"http://www.w3.org/1999/xhtml\" class=\"Competence\">manage and transform work or study contexts that are complex, unpredictable and require new strategic approaches <html:br></html:br>take responsibility for contributing to professional knowledge and practice and/or for reviewing the strategic performance of teams"
-      }
-    },
-    "targetFrameworkName" : {
-      "text" : {
-        "@content-type" : "text/plain",
-        "@lang" : "en",
-        "#text" : "European Qualifications Framework for lifelong learning - (2008/C 111/01)"
-      }
-    },
-    "credentialStatus" : {
-      "id" : "https://essif.europa.eu/status/43",
-      "type" : "CredentialsStatusList2020"
-    },
-    "credentialSchema" : {
-      "id" : "https://essif.europa.eu/tsr-123/verifiableattestation.json",
-      "type" : "JsonSchemaValidator2018"
-    },
-    "evidence" : {
-      "id" : "https://essif.europa.eu/evidence/f2aeec97-fc0d-42bf-8ca7-0548192d4231",
-      "type" : [ "DocumentVerification, Assessment" ],
-      "verifier" : "https:// essif.europa.eu /issuers/48",
-      "evidenceDocument" : [ "Passport", "Assessment" ]
-    },
-    "proof" : {
-      "type" : "EcdsaSecp256k1Signature2019",
-      "created" : "2021-07-19T07:43:36Z",
-      "creator" : "did:ebsi:22RgRjvk6mVkUaZohDtDSweQFfMXTh1YBnCZu3JgPNEmx7zZ",
-      "jws" : "eyJiNjQiOmZhbHNlLCJjcml0IjpbImI2NCJdLCJhbGciOiJFUzI1NksifQ..rlHLKG45ZXX8yRw2pynVs4H79ZM2BOq1WqF7M42OwCIOoJVi4V31eTJCNMnANsgzWrDOTsCUfj8EAt4fvEKylA"
+    init {
+        PolicyRegistry.register(SignaturePolicy())
+        PolicyRegistry.register(TrustedIssuerDidPolicy())
+        PolicyRegistry.register(TrustedSubjectDidPolicy())
+        PolicyRegistry.register(JsonSchemaPolicy())
     }
-  } ],
-  "proof" : {
-    "type" : "Ed25519Signature2018",
-    "creator" : "did:key:z6MkuHT1Q4FTSC9tqaU3bt7dCGesYNzjNMfYX1YUvWKXLpod",
-    "created" : "2021-07-19T07:45:50Z",
-    "jws" : "eyJiNjQiOmZhbHNlLCJjcml0IjpbImI2NCJdLCJhbGciOiJFZERTQSJ9..uXAYH1UQL3D5YUoJRYLFbNsM5ojL0LVAWTVD3BRVu5Sieu9N3ueYbfN6y6TAPLNkMT5Ogs6Nu9M0O2U2gJdUCQ"
-  }
-}
-    """, listOf(SignaturePolicy(), JsonSchemaPolicy())
-    )
-
-    println(res)
 }
---
+++ src/test/kotlin/id/walt/auditor/AuditorTest.kt
@@ -0,0 +1,61 @@
+package id.walt.auditor
+
+import id.walt.auditor.AuditorService
+import id.walt.auditor.JsonSchemaPolicy
+import id.walt.auditor.SignaturePolicy
+import id.walt.model.DidMethod
+import id.walt.servicematrix.ServiceMatrix
+import id.walt.services.did.DidService
+import id.walt.services.vc.JsonLdCredentialService
+import id.walt.signatory.ProofConfig
+import id.walt.signatory.Signatory
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldBeSameSizeAs
+import io.kotest.matchers.collections.shouldContainAll
+import io.kotest.matchers.shouldBe
+
+private lateinit var did: String
+private lateinit var vcStr: String
+private lateinit var vpStr: String
+
+class AuditorCommandTest : StringSpec({
+    ServiceMatrix("service-matrix.properties")
+
+    val signatory = Signatory.getService()
+    val credentialService = JsonLdCredentialService.getService()
+
+    "1. generate key" {
+        did = DidService.create(DidMethod.key)
+
+        println("Generated: $did")
+    }
+
+    "2. issue vc" {
+        vcStr = signatory.issue(
+            "Europass", ProofConfig(
+                issuerDid = did,
+                subjectDid = did,
+                issuerVerificationMethod = "Ed25519Signature2018"
+            )
+        )
+    }
+
+    "3. present vc" {
+        vpStr = credentialService.present(vcStr, "https://api.preprod.ebsi.eu", "d04442d3-661f-411e-a80f-42f19f594c9d")
+    }
+
+    "4. verify vp" {
+        val res = AuditorService.verify(vpStr, listOf(SignaturePolicy(), JsonSchemaPolicy()))
+
+        res.overallStatus shouldBe true
+
+        res.policyResults.keys shouldBeSameSizeAs listOf(SignaturePolicy(), JsonSchemaPolicy())
+
+        res.policyResults.keys.map { it.id() } shouldContainAll
+                listOf(SignaturePolicy(), JsonSchemaPolicy()).map { it.id() }
+
+        res.policyResults.values.forEach {
+            it shouldBe true
+        }
+    }
+})
