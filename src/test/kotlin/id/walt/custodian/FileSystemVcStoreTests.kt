56ccad60d4bc6f47ffc74ff9b846771e56e39c83
Kevin Burgmann <kevin@walt.id>
2021-08-20 03:10:35 +0200

    Added file system VC store tests

---
+++ src/test/kotlin/id/walt/custodian/FileSystemVcStoreTests.kt
@@ -0,0 +1,55 @@
+package id.walt.custodian
+
+import id.walt.servicematrix.ServiceMatrix
+import id.walt.servicematrix.ServiceRegistry
+import id.walt.services.vcstore.FileSystemVcStoreService
+import id.walt.services.vcstore.VcStoreService
+import id.walt.vclib.vclist.Europass
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.ints.shouldBeGreaterThanOrEqual
+import io.kotest.matchers.shouldBe
+
+class FileSystemVcStoreTests : StringSpec({
+
+    ServiceMatrix("service-matrix.properties")
+    val custodian = CustodianService.getService()
+    ServiceRegistry.registerService<VcStoreService>(FileSystemVcStoreService())
+
+    val vc = Europass.template!!.invoke()
+
+    "1: Store credential" {
+        custodian.storeCredential("my-test-europass", vc)
+
+        custodian.listCredentialIds().size shouldBeGreaterThanOrEqual 1
+    }
+
+    "2: List credential ids" {
+        custodian.listCredentialIds().forEach {
+            println(it)
+        }
+    }
+
+    "3: Retrieve credential" {
+        val retrievedVc = custodian.getCredential("my-test-europass")
+        println(retrievedVc)
+        retrievedVc as Europass
+        println(retrievedVc.credentialSchema!!.id)
+    }
+
+    "3: List credentials" {
+        custodian.listCredentials().contains(vc) shouldBe true
+    }
+
+    "4: Delete credentials" {
+        custodian.listCredentialIds().forEach {
+            custodian.deleteCredential(it)
+        }
+
+        custodian.listCredentialIds().forEach {
+            println("! Remaining credential id: $it")
+        }
+
+        custodian.listCredentialIds().size shouldBe 0
+        custodian.listCredentials().size shouldBe 0
+    }
+})
