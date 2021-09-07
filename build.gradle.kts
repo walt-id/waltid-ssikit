c151fd9269fca36d56fcdd6e23f5d1d1ff9d71fc
Kevin Burgmann <kevin@walt.id>
2021-09-07 18:07:56 +0200

    Update ktor versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,8 +62,8 @@ dependencies {
 
     // HTTP
     implementation("io.ktor:ktor-client-core:1.6.3")
-    implementation("io.ktor:ktor-client-cio:1.6.2")
-    implementation("io.ktor:ktor-client-serialization:1.6.2")
+    implementation("io.ktor:ktor-client-cio:1.6.3")
+    implementation("io.ktor:ktor-client-serialization:1.6.3")
     implementation("io.ktor:ktor-client-logging:1.6.2")
 
     // REST
