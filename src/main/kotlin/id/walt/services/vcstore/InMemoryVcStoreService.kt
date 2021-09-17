6c911b9a00463e322f11e7cdbae7f9ca33d39753
Severin Stampler <severin.stampler@gmail.com>
2021-09-17 12:42:51 +0200

    fix vc store test after grouping change

--- src/main/kotlin/id/walt/services/vcstore/InMemoryVcStoreService.kt
+++ src/main/kotlin/id/walt/services/vcstore/InMemoryVcStoreService.kt
@@ -1,6 +1,7 @@
 package id.walt.services.vcstore
 
 import id.walt.vclib.model.VerifiableCredential
+import java.nio.file.Path
 
 open class InMemoryVcStoreService : VcStoreService() {
 
@@ -10,9 +11,9 @@ open class InMemoryVcStoreService : VcStoreService() {
 
     override fun getCredential(id: String, group: String): VerifiableCredential = store[getKey(id, group)]!!
 
-    override fun listCredentials(group: String): List<VerifiableCredential> = listCredentialIds(group).map { store[it]!! }.toList()
+    override fun listCredentials(group: String): List<VerifiableCredential> = listCredentialIds(group).map { store[getKey(it, group)]!! }.toList()
 
-    override fun listCredentialIds(group: String): List<String> = store.keys.filter { it.startsWith("${group}/") }.toList()
+    override fun listCredentialIds(group: String): List<String> = store.keys.filter { it.startsWith("${group}/") }.map {Path.of(it).fileName.toString() }.toList()
 
     override fun storeCredential(alias: String, vc: VerifiableCredential, group: String) {
         store[getKey(alias, group)] = vc
