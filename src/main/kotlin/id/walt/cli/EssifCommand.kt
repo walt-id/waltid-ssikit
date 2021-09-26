5e470e5d3e92574b3279d9ca11f448104bd2483a
Philipp Potisk <phil@walt.id>
2021-09-26 20:47:33 +0200

    Updated essif cli dialogues

--- src/main/kotlin/id/walt/cli/EssifCommand.kt
+++ src/main/kotlin/id/walt/cli/EssifCommand.kt
@@ -2,43 +2,16 @@ package id.walt.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
-import com.github.ajalt.clikt.parameters.arguments.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.file
 import id.walt.Values
-import id.walt.common.readWhenContent
 import id.walt.model.DidMethod
 import id.walt.services.did.DidService
 import id.walt.services.essif.EssifClient
 import id.walt.services.essif.EssifClientVcExchange
 import id.walt.services.essif.TrustedIssuerClient
-import id.walt.services.essif.didebsi.DidEbsiService
 import java.io.File
-
-// TODO: Support following commands
-
-// essif login -> Verifiable Authorization
-
-// essif auth -> Access Token
-
-// essif did list
-// essif did --did DID:ebsi:0987654
-// essif did register
-
-// essif tir list
-// essif tir --did DID:ebsi:0987654
-// essif tir register
-
-// essif taor list
-// essif taor --did DID:ebsi:0987654
-// essif taor register
-
-// essif tsr list
-// essif tsr --id 12345
-// essif tsr register
-
-// maybe: "registries"
 
 class EssifCommand : CliktCommand(
     name = "essif",
@@ -58,14 +31,14 @@ class EssifOnboardingCommand : CliktCommand(
         For gaining access to the EBSI service, a bearer token from 
         https://app.preprod.ebsi.eu/users-onboarding must be present."""
 ) {
-    val bearerTokenFile: File by argument("BEARER-TOKEN-FILE", help = "File containing the Bearer Token from EOS").file().default(File("data/ebsi/bearer-token.txt"))
+    val bearerTokenFile: File by argument("BEARER-TOKEN-FILE", help = "File containing the bearer token from EOS").file()
     val did: String by option("-d", "--did", help = "DID to be onboarded").required()
 
     override fun run() {
 
-        echo("ESSIF onboarding of DID $did ...\n")
+        echo("ESSIF onboarding of $did\n")
 
-        EssifClient.onboard(did, readWhenContent(bearerTokenFile).replace("\n", ""))
+        EssifClient.onboard(did, bearerTokenFile.readText().replace("\n", ""))
 
         echo("ESSIF onboarding for DID $did was performed successfully.")
     }
@@ -108,13 +81,11 @@ class EssifDidRegisterCommand : CliktCommand(
     val did: String by option("-d", "--did", help = "DID to be onboarded").required()
     val ethKeyAlias: String? by option("-k", "--eth-key", help = "Key to be used for signing the ETH transaction")
 
-    private val didEbsiService = DidEbsiService.getService()
-
     override fun run() {
 
-        echo("Registering DID $did on the EBSI ledger using key $ethKeyAlias ...\n")
+        echo("Registering DID $did on the EBSI ledger.\n")
 
-        didEbsiService.registerDid(did, ethKeyAlias ?: did)
+        EssifClient.registerDid(did, ethKeyAlias ?: did)
 
         echo("DID registration was performed successfully. Call command: 'did resolve --did $did' in order to retrieve the DID document from the EBSI ledger.")
     }
