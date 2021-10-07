4cda55a031856d3734794ad5085723b70063adef
Alfred Tuffreau <alfred.tuffreau@bcdiploma.com>
2021-10-07 15:50:49 +0200

    In memory Keystore service + UT + InMemoryHKVStore fixes

--- src/main/kotlin/id/walt/services/hkvstore/InMemoryHKVStore.kt
+++ src/main/kotlin/id/walt/services/hkvstore/InMemoryHKVStore.kt
@@ -1,8 +1,11 @@
 package id.walt.services.hkvstore
 
-import java.nio.file.Path
+import kotlin.collections.HashMap
 
 open class InMemoryHKVStore : HKVStoreService() {
+
+    companion object { val aliasKey = HKVKey("keys", "alias") }
+
     val store = HashMap<HKVKey, ByteArray>()
 
     override fun put(key: HKVKey, value: ByteArray) = store.set(key, value)
@@ -11,13 +14,17 @@ open class InMemoryHKVStore : HKVStoreService() {
 
     override fun listChildKeys(parent: HKVKey, recursive: Boolean): Set<HKVKey> = when {
         recursive -> store.keys.filter { it.startsWith(parent) }.toSet()
-        else -> store.keys.filter { it.parent == parent }.toSet()
+        else -> store.keys.filter { it.parent != null && it.parent != aliasKey }.map { it.parent!! }.toSet()
     }
 
     override fun delete(key: HKVKey, recursive: Boolean): Boolean {
         if (recursive)
             listChildKeys(key, true).forEach { store.remove(it) }
+        listKeyAliases(key).forEach { store.remove(it) }
         store.remove(key)
         return true
     }
+
+    private fun listKeyAliases(key: HKVKey) = store.keys
+        .filter { it.parent == aliasKey }.filter { getAsString(it) == key.name }.toSet()
 }
---
+++ src/test/kotlin/id/walt/services/keystore/InMemoryKeyStoreServiceTest.kt
@@ -0,0 +1,40 @@
+package id.walt.services.keystore
+
+import id.walt.crypto.Key
+import id.walt.crypto.KeyAlgorithm
+import id.walt.crypto.keyPairGeneratorSecp256k1
+import id.walt.crypto.newKeyId
+import id.walt.services.CryptoProvider
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeSameInstanceAs
+
+class InMemoryKeyStoreServiceTest : StringSpec({
+    "In memory key store can store keys, add aliases, list, get and delete keys by id" {
+        val inMemoryKeyStore = InMemoryKeyStoreService()
+        val key = Key(
+            newKeyId(),
+            KeyAlgorithm.ECDSA_Secp256k1,
+            CryptoProvider.SUN,
+            keyPairGeneratorSecp256k1().generateKeyPair()
+        )
+        val alias = "my-test-alias"
+
+        inMemoryKeyStore.store(key)
+        inMemoryKeyStore.listKeys() shouldBe listOf(key)
+        inMemoryKeyStore.load(key.keyId.id) shouldBe key
+
+        inMemoryKeyStore.addAlias(key.keyId, alias)
+        inMemoryKeyStore.listKeys() shouldBe listOf(key)
+        inMemoryKeyStore.getKeyId(alias) shouldBe key.keyId.id
+        inMemoryKeyStore.load(alias) shouldBe key
+
+        inMemoryKeyStore.delete(key.keyId.id)
+        inMemoryKeyStore.listKeys() shouldBe emptyList()
+        inMemoryKeyStore.getKeyId(key.keyId.id) shouldBe null
+        inMemoryKeyStore.getKeyId(alias) shouldBe null
+        shouldThrow<NullPointerException> { inMemoryKeyStore.load(key.keyId.id) }
+        shouldThrow<NullPointerException> { inMemoryKeyStore.load(alias) }
+    }
+})
