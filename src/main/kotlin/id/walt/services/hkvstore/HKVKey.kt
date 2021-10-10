224436ec4838dbad9859141aed332732acafc6c7
Kevin Burgmann <kevin@walt.id>
2021-10-10 19:08:00 +0200

    Cleanup HKVKey

--- src/main/kotlin/id/walt/services/hkvstore/HKVKey.kt
+++ src/main/kotlin/id/walt/services/hkvstore/HKVKey.kt
@@ -1,74 +1,57 @@
 package id.walt.services.hkvstore
 
-import java.net.URL
 import java.net.URLDecoder
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 import java.nio.file.Path
 import java.util.*
 
-class HKVKey (
+class HKVKey(
     private val rootKey: String,
     vararg moreKeys: String
-    ) {
+) {
 
     private val subKeys = LinkedList<String>()
 
     init {
-        if(moreKeys.isNotEmpty())
+        if (moreKeys.isNotEmpty())
             subKeys.addAll(moreKeys)
     }
 
     private fun forFS(value: String): String = URLEncoder.encode(value, StandardCharsets.UTF_8)
 
-    fun toPath(): Path {
-        return Path.of(forFS(rootKey), *subKeys.map { forFS(it) }.toTypedArray())
-    }
+    fun toPath(): Path = Path.of(forFS(rootKey), *subKeys.map { forFS(it) }.toTypedArray())
 
-    override fun toString(): String {
-        return "/${rootKey}/${subKeys.joinToString("/")}"
-    }
+    override fun toString(): String = "/${rootKey}/${subKeys.joinToString("/")}"
 
-    override fun equals(other: Any?): Boolean {
-        if(other != null) {
-            return other.toString() == toString()
-        } else
-            return false
-    }
+    override fun equals(other: Any?): Boolean = if (other == null) false else other.toString() == toString()
 
-    override fun hashCode(): Int {
-        return toString().hashCode()
-    }
+    override fun hashCode(): Int = toString().hashCode()
 
     fun startsWith(key: HKVKey): Boolean {
         return toString().startsWith(key.toString())
     }
 
     val name: String
-        get() = when(subKeys.isNotEmpty()) {
-            true -> subKeys.last
-            else -> rootKey
-        }
+        get() = if (subKeys.isEmpty()) rootKey else subKeys.last
 
     val parent: HKVKey?
-        get() = when(subKeys.isNotEmpty()) {
-            true -> HKVKey(rootKey, *subKeys.subList(0, subKeys.count() - 1).toTypedArray())
-            else -> null
-        }
+        get() = if (subKeys.isEmpty()) null else HKVKey(rootKey, *subKeys.subList(0, subKeys.size - 1).toTypedArray())
 
     companion object {
-        private fun fromFS(value: String): String {
-            return URLDecoder.decode(value, StandardCharsets.UTF_8)
-        }
+        private fun fromFS(value: String): String = URLDecoder.decode(value, StandardCharsets.UTF_8)
 
         fun fromPath(path: Path): HKVKey {
-            return HKVKey(fromFS(path.getName(0).toString()), *path.subpath(1, path.nameCount).map { fromFS(it.toString()) }.toTypedArray())
+            return HKVKey(
+                fromFS(path.getName(0).toString()),
+                *path.subpath(1, path.nameCount).map { fromFS(it.toString()) }.toTypedArray()
+            )
         }
 
         fun fromString(path: String): HKVKey {
             val parts = path.split("/")
             val rootKey = parts[0]
-            return HKVKey(rootKey, *parts.subList(1, parts.count()).toTypedArray())
+            return HKVKey(rootKey, *parts.subList(1, parts.size).toTypedArray())
         }
     }
 }
