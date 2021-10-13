d5b3d107844ac2a737f2a2586433fc95dd54a05b
severinstampler <severin@walt.id>
2021-10-13 11:53:21 +0200

    simple test to prove context switching

---
+++ src/test/kotlin/id/walt/context/TestContext.kt
@@ -0,0 +1,32 @@
+package id.walt.context
+
+import id.walt.services.context.WaltContext
+import id.walt.services.hkvstore.FileSystemHKVStore
+import id.walt.services.hkvstore.FilesystemStoreConfig
+import id.walt.services.hkvstore.HKVStoreService
+import id.walt.services.keystore.HKVKeyStoreService
+import id.walt.services.keystore.KeyStoreService
+import id.walt.services.vcstore.HKVVcStoreService
+import id.walt.services.vcstore.VcStoreService
+
+val TEST_CONTEXT_DATA_ROOT = "testdata/context"
+
+class TestContext(var currentContext: String): WaltContext() {
+
+  val userContexts = mapOf(
+    Pair("userA", FileSystemHKVStore(FilesystemStoreConfig(dataRoot = "${id.walt.context.TEST_CONTEXT_DATA_ROOT}/userA"))),
+    Pair("userB",  FileSystemHKVStore(FilesystemStoreConfig(dataRoot = "${id.walt.context.TEST_CONTEXT_DATA_ROOT}/userB")))
+  )
+
+  val keyStore = HKVKeyStoreService()
+  val vcStore = HKVVcStoreService()
+
+  override fun getKeyStore(): KeyStoreService = keyStore
+
+  override fun getVcStore(): VcStoreService = vcStore
+
+  override fun getHKVStore(): HKVStoreService {
+    return userContexts.get(currentContext)!!
+  }
+
+}
---
+++ src/test/kotlin/id/walt/context/WaltContextTest.kt
@@ -0,0 +1,46 @@
+package id.walt.context
+
+import id.walt.model.DidMethod
+import id.walt.servicematrix.ServiceMatrix
+import id.walt.servicematrix.ServiceRegistry
+import id.walt.services.context.WaltContext
+import id.walt.services.did.DidService
+import id.walt.test.RESOURCES_PATH
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import java.io.File
+
+class WaltContextTest: AnnotationSpec() {
+
+  @BeforeAll
+  fun setup() {
+    ServiceMatrix("$RESOURCES_PATH/service-matrix.properties")
+    File(TEST_CONTEXT_DATA_ROOT).deleteRecursively()
+  }
+
+  @AfterAll
+  fun cleanup() {
+    //File(TEST_CONTEXT_DATA_ROOT).deleteRecursively()
+  }
+
+  @Test
+  fun testContext() {
+    var context = TestContext("userA")
+    ServiceRegistry.registerService<WaltContext>(context)
+    val did1 = DidService.create(DidMethod.key)
+
+    context.currentContext = "userB"
+    val did2 = DidService.create(DidMethod.key)
+
+    val didList2 = DidService.listDids()
+    didList2 shouldHaveSize 1
+    didList2.first() shouldBe did2
+
+    context.currentContext = "userA"
+
+    val didList1 = DidService.listDids()
+    didList1 shouldHaveSize 1
+    didList1.first() shouldBe did1
+  }
+}
