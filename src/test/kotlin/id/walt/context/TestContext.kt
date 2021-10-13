607e32fbae82d7a2429abcf2dccd506857115a9f
Philipp Potisk <phil@walt.id>
2021-10-13 12:20:50 +0200

    Moved testdata dir to build-dir

--- src/test/kotlin/id/walt/context/TestContext.kt
+++ src/test/kotlin/id/walt/context/TestContext.kt
@@ -9,7 +9,7 @@ import id.walt.services.keystore.KeyStoreService
 import id.walt.services.vcstore.HKVVcStoreService
 import id.walt.services.vcstore.VcStoreService
 
-val TEST_CONTEXT_DATA_ROOT = "testdata/context"
+val TEST_CONTEXT_DATA_ROOT = "build/testdata/context"
 
 class TestContext(var currentContext: String): WaltContext() {
 
