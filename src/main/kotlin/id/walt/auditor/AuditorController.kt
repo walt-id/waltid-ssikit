7ae8980e9638fb4c69fda176f3131ad5289158fd
Philipp Potisk <phil@walt.id>
2021-10-23 11:16:00 +0200

    Auditor REST API now takes a list of policies

--- src/main/kotlin/id/walt/auditor/AuditorController.kt
+++ src/main/kotlin/id/walt/auditor/AuditorController.kt
@@ -15,7 +15,9 @@ object AuditorController {
     }.json<Array<VerificationPolicy>>("200")
 
     fun verifyVP(ctx: Context) {
-        val policies = ctx.queryParams("policy").ifEmpty { listOf(PolicyRegistry.defaultPolicyId) }
+
+        val policyList = ctx.queryParams("policyList")?.let { it[0].split(",").map { it.trim() } }
+        val policies = policyList.ifEmpty { listOf(PolicyRegistry.defaultPolicyId) }
         if (policies.any { !PolicyRegistry.contains(it) }) {
             ctx.status(HttpStatus.SC_BAD_REQUEST).result("Unknown policy given")
         } else {
@@ -25,8 +27,9 @@ object AuditorController {
         }
     }
 
-    fun verifyVPDocs() = document().operation {
-        it.summary("Verify a W3C VerifiablePresentation").operationId("verifyVP").addTagsItem("Verification Policies")
-    }.body<String> { it.description("VP to be verified") }
+    fun verifyVPDocs() = document()
+        .operation { it.summary("Verify a W3C VerifiableCredential or VerifiablePresentation").operationId("verifyVP").addTagsItem("Verification Policies") }
+        .body<String> { it.description("VC or VP to be verified") }
+        .queryParam<String>("policyList") { it.description("Optional comma-separated list for setting the policies to be verified.") }
         .json<String>("200") { it.description("Request processed successfully (VP might not be valid)") }
 }
