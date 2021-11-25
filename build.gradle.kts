be1be482423f373f7cf35fcb5ec9ff959802dd10
Philipp Potisk <phil@walt.id>
2021-11-25 22:26:05 +0100

    Set version to 1.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "id.walt"
-version = "1.1.0"
+version = "1.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/id/walt/Values.kt
+++ src/main/kotlin/id/walt/Values.kt
@@ -1,6 +1,6 @@
 package id.walt
 
 object Values {
-    const val version = "1.1.0"
+    const val version = "1.1-SNAPSHOT"
     const val isSnapshot = true
 }
