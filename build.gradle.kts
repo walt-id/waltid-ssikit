e1cb0f1724ee5c5c56c3bf29024cceca1ea15e24
Kevin Burgmann <kevin@walt.id>
2021-12-12 20:21:46 +0100

    Revert "Update versioning information"
    This reverts commit 6701ce92a6935bdba5e3536f25a9d2018cb788a8.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "id.walt"
-version = "1.2-SNAPSHOT"
+version = "1.2.0"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/id/walt/Values.kt
+++ src/main/kotlin/id/walt/Values.kt
@@ -1,6 +1,6 @@
 package id.walt
 
 object Values {
-    const val version = "1.2-SNAPSHOT"
+    const val version = "1.2.0"
     const val isSnapshot = true
 }
