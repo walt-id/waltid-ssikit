489f5c583331d77f01b7c2d74f632937be06b13f
Kevin Burgmann <kevin@walt.id>
2021-12-14 00:14:00 +0100

    Rename PrivateKeyHandle to ECPrivateKeyHandle, add RSAPrivateKeyHandle

--- src/main/kotlin/id/walt/crypto/EcdsaSecp256k1Signature2019LdSigner.kt
+++ src/main/kotlin/id/walt/crypto/EcdsaSecp256k1Signature2019LdSigner.kt
@@ -12,18 +12,15 @@ import info.weboftrust.ldsignatures.suites.SignatureSuites
 import info.weboftrust.ldsignatures.util.JWSUtil
 
 @Deprecated(message = "Replaced with LdSigner")
-class EcdsaSecp256k1Signature2019LdSigner(val keyId: KeyId) :
-    LdSigner<EcdsaSecp256k1Signature2019SignatureSuite?>(
-        SignatureSuites.SIGNATURE_SUITE_ECDSASECP256L1SIGNATURE2019,
-        null,
-        Canonicalizers.CANONICALIZER_JCSCANONICALIZER
-    ) {
+class EcdsaSecp256k1Signature2019LdSigner(val keyId: KeyId) : LdSigner<EcdsaSecp256k1Signature2019SignatureSuite?>(
+    SignatureSuites.SIGNATURE_SUITE_ECDSASECP256L1SIGNATURE2019, null, Canonicalizers.CANONICALIZER_JCSCANONICALIZER
+) {
 
     override fun sign(ldProofBuilder: LdProof.Builder<*>, signingInput: ByteArray) {
         val jwsHeader =
             JWSHeader.Builder(JWSAlgorithm.ES256K).base64URLEncodePayload(false).criticalParams(setOf("b64")).build()
         val jwsSigningInput = JWSUtil.getJwsSigningInput(jwsHeader, signingInput)
-        val jwsSigner = ECDSASigner(PrivateKeyHandle(keyId), Curve.SECP256K1)
+        val jwsSigner = ECDSASigner(ECPrivateKeyHandle(keyId), Curve.SECP256K1)
         jwsSigner.jcaContext.provider = WaltIdProvider()
         val signature = jwsSigner.sign(jwsHeader, jwsSigningInput)
         val jws = JWSUtil.serializeDetachedJws(jwsHeader, signature)
--- src/main/kotlin/id/walt/crypto/PrivateKeyHandle.kt
+++ src/main/kotlin/id/walt/crypto/PrivateKeyHandle.kt
@@ -3,9 +3,23 @@ package id.walt.crypto
 import java.security.GeneralSecurityException
 import java.security.PrivateKey
 
-class PrivateKeyHandle(val keyId: KeyId) : PrivateKey {
+class ECPrivateKeyHandle(val keyId: KeyId) : PrivateKey {
     override fun getAlgorithm(): String {
         return "EC"
+    }
+
+    override fun getFormat(): String {
+        throw GeneralSecurityException("Not supported, as private keys will not be exposed.")
+    }
+
+    override fun getEncoded(): ByteArray {
+        throw GeneralSecurityException("Not supported, as private keys will not be exposed.")
+    }
+}
+
+class RSAPrivateKeyHandle(val keyId: KeyId) : PrivateKey {
+    override fun getAlgorithm(): String {
+        return "RSA"
     }
 
     override fun getFormat(): String {
