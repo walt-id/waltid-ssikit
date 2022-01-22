8f7af8e1d3237960f7767787a68d2b6f886261f4
Kevin Burgmann <kevin@walt.id>
2022-01-22 02:17:04 +0100

    Add tests for RSA & JWK KeyCommand

--- src/test/kotlin/id/walt/cli/KeyCommandTest.kt
+++ src/test/kotlin/id/walt/cli/KeyCommandTest.kt
@@ -3,6 +3,8 @@ package id.walt.cli
 import com.github.ajalt.clikt.core.PrintHelpMessage
 import id.walt.crypto.KeyAlgorithm
 import id.walt.servicematrix.ServiceMatrix
+import id.walt.servicematrix.ServiceRegistry
+import id.walt.services.key.InMemoryKeyService
 import id.walt.services.key.KeyService
 import id.walt.test.RESOURCES_PATH
 import io.kotest.assertions.throwables.shouldThrow
@@ -14,7 +16,10 @@ class KeyCommandTest : StringSpec({
 
     ServiceMatrix("$RESOURCES_PATH/service-matrix.properties")
 
-    "key gen --help" {
+    val keyService = KeyService.getService()
+    // ServiceRegistry.registerService<KIeyService>(InMemoryKeyService())
+
+    "1. key gen --help" {
         val e = shouldThrow<PrintHelpMessage> {
             GenKeyCommand().parse(listOf("--help"))
         }
@@ -22,44 +27,51 @@ class KeyCommandTest : StringSpec({
         message shouldContain "-a, --algorithm [Ed25519|Secp256k1|RSA]"
     }
 
-    "key gen Ed25519" {
+    "2. key gen Ed25519" {
         GenKeyCommand().parse(listOf("-a", "Ed25519"))
     }
 
-    "key gen Secp256k1" {
+    "3. key gen Secp256k1" {
         GenKeyCommand().parse(listOf("-a", "Secp256k1"))
     }
 
-    "key gen RSA" {
+    "4. key gen RSA" {
         GenKeyCommand().parse(listOf("-a", "RSA"))
     }
 
-    "key export Secp256k1" {
+    "5. key export Secp256k1" {
         val key = KeyService.getService().generate(KeyAlgorithm.ECDSA_Secp256k1)
         ExportKeyCommand().parse(listOf(key.id))
     }
 
-    "key export Ed25519" {
+    "6. key export Ed25519" {
         val key = KeyService.getService().generate(KeyAlgorithm.EdDSA_Ed25519)
         ExportKeyCommand().parse(listOf(key.id))
     }
 
-    "key import Ed25519 priv key JWK" {
+    "7. key import Ed25519 priv key JWK" {
         ImportKeyCommand().parse(listOf("src/test/resources/cli/privKeyEd25519Jwk.json"))
-        KeyService.getService().delete("45674a4ac169f7f4716804393d20480138a")
+        keyService.delete("45674a4ac169f7f4716804393d20480138a")
     }
 
-    "key import Ed25519 pub key JWK" {
+    "8. key import Ed25519 pub key JWK" {
         ImportKeyCommand().parse(listOf("src/test/resources/cli/pubKeyEd25519Jwk.json"))
-        KeyService.getService().delete("12374a4ac169f7f4716804393d20480138a")
+        keyService.delete("12374a4ac169f7f4716804393d20480138a")
     }
 
-// Import in PEM format currently not supported
-//    "key import Ed25519 priv key PEM" {
-//        ImportKeyCommand().parse(listOf("src/test/resources/cli/privKeyEd25519Pem.txt", "src/test/resources/cli/pubKeyEd25519Pem.txt"))
-//    }
-//
-//    "key import Ed25519 pub key PEM" {
-//        ImportKeyCommand().parse(listOf("src/test/resources/cli/pubKeyEd25519Pem.txt"))
-//    }
+    "9. key import RSA priv key PEM" {
+        ImportKeyCommand().parse(listOf("src/test/resources/key/privkey.pem"))
+    }
+
+    "10. key import RSA pub key PEM" {
+        ImportKeyCommand().parse(listOf("src/test/resources/key/pubkey.pem"))
+    }
+
+    "11. key import RSA priv key JWK" {
+        ImportKeyCommand().parse(listOf("src/test/resources/key/privkey.jwk"))
+    }
+
+    "12. clear keys" {
+        keyService.listKeys().forEach { keyService.delete(it.keyId.toString()) }
+    }
 })
