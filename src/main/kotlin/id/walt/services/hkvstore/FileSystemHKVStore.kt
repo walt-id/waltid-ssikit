fe2cb01c525ca6198ef44e7706be8cd8d691288d
Kevin Burgmann <kevin@walt.id>
2022-01-30 21:58:49 +0100

    Hash HKV keys using Keccak/SHA3-512 when MAX_KEY_SIZE is exceeded

--- src/main/kotlin/id/walt/services/hkvstore/FileSystemHKVStore.kt
+++ src/main/kotlin/id/walt/services/hkvstore/FileSystemHKVStore.kt
@@ -1,7 +1,9 @@
 package id.walt.services.hkvstore
 
 import id.walt.servicematrix.ServiceConfiguration
+import io.ipfs.multibase.binary.Base32
 import io.ktor.util.*
+import org.apache.commons.codec.digest.DigestUtils
 import java.io.File
 import java.nio.file.Path
 
@@ -16,30 +18,46 @@ class FileSystemHKVStore(configPath: String) : HKVStoreService() {
     override lateinit var configuration: FilesystemStoreConfig
 
     init {
-      if (configPath.isNotEmpty())
-          configuration = fromConfiguration(configPath)
+        if (configPath.isNotEmpty()) configuration = fromConfiguration(configPath)
     }
 
     constructor(config: FilesystemStoreConfig) : this("") {
         configuration = config
     }
 
+    private fun getFinalPath(keyPath: Path): File = dataDirCombinePath(keyPath).apply {
+        if (name.length > MAX_KEY_SIZE) {
+            val hashedFileNameBytes = DigestUtils.sha3_512(nameWithoutExtension)
+            val hashedFileName = Base32().encodeToString(hashedFileNameBytes).replace("=", "").replace("+", "")
+
+            val ext = extension
+
+            val newName = hashedFileName + (ext.ifBlank { "" })
+
+            val newPath = keyPath.parent.resolve(newName)
+
+            println("NEW PATH IS $newPath")
+
+            return dataDirCombinePath(newPath)
+        }
+    }
+
     override fun put(key: HKVKey, value: ByteArray) {
-        dataDirCombinePath(key.toPath()).apply {
+        getFinalPath(key.toPath()).apply {
             parentFile.mkdirs()
             writeBytes(value)
         }
     }
 
-    override fun getAsByteArray(key: HKVKey): ByteArray? = dataDirCombinePath(key.toPath()).run {
-        return when(exists()) {
+    override fun getAsByteArray(key: HKVKey): ByteArray? = getFinalPath(key.toPath()).run {
+        return when (exists()) {
             true -> readBytes()
             else -> null
         }
     }
 
     override fun listChildKeys(parent: HKVKey, recursive: Boolean): Set<HKVKey> =
-        dataDirCombinePath(parent.toPath()).listFiles().let { pathFileList ->
+        getFinalPath(parent.toPath()).listFiles().let { pathFileList ->
             when (recursive) {
                 false -> pathFileList?.filter { it.isFile }?.map { HKVKey.fromPath(dataDirRelativePath(it)) }?.toSet()
                 true -> pathFileList?.flatMap {
@@ -54,8 +72,12 @@ class FileSystemHKVStore(configPath: String) : HKVStoreService() {
         }
 
     override fun delete(key: HKVKey, recursive: Boolean): Boolean =
-        dataDirCombinePath(key.toPath()).run { if (recursive) deleteRecursively() else delete() }
+        getFinalPath(key.toPath()).run { if (recursive) deleteRecursively() else delete() }
 
     private fun dataDirRelativePath(file: File) = configuration.dataDirectory.relativize(file.toPath())
     private fun dataDirCombinePath(key: Path) = configuration.dataDirectory.combineSafe(key)
+
+    companion object {
+        private const val MAX_KEY_SIZE = 120
+    }
 }
