19e5da0ca4ac3bcf1d324b504e5f41c53c828348
severinstampler <severin@walt.id>
2022-02-17 12:51:52 +0100

    up snapshot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "id.walt"
-version = "1.6-SNAPSHOT"
+version = "1.7-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -45,7 +45,7 @@ dependencies {
     implementation("com.google.guava:guava:31.0.1-jre")
 
     // VC
-    implementation("id.walt:waltid-ssikit-vclib:1.15-SNAPSHOT")
+    implementation("id.walt:waltid-ssikit-vclib:1.15.0")
 
     // JSON
     implementation("org.json:json:20211205")
