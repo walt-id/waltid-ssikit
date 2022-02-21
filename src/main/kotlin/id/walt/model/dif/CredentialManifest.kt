924b95fef3d03197bc6a9e482cb4ccea4d2230cc
severinstampler <severin@walt.id>
2022-02-21 17:56:03 +0100

    possibility to add presentation definition to credential manifest

--- src/main/kotlin/id/walt/model/dif/CredentialManifest.kt
+++ src/main/kotlin/id/walt/model/dif/CredentialManifest.kt
@@ -4,5 +4,8 @@ import com.beust.klaxon.Json
 
 data class CredentialManifest(
     val issuer: Issuer,
-    @Json(name = "output_descriptors") val outputDescriptors: List<OutputDescriptor>
+    @Json(name = "output_descriptors")
+    val outputDescriptors: List<OutputDescriptor>,
+    @Json(name = "presentation_definition", serializeNull = false)
+    val presentationDefinition: PresentationDefinition? = null
 )
