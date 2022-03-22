4299de829307b71967907eccd9145e78ab8d2ae9
Philipp Potisk <phil@walt.id>
2022-03-22 22:50:56 +0100

    Set to snapshot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "id.walt"
-version = "1.8.0"
+version = "1.8.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/id/walt/Values.kt
+++ src/main/kotlin/id/walt/Values.kt
@@ -1,6 +1,6 @@
 package id.walt
 
 object Values {
-    const val version = "1.8.0"
+    const val version = "1.8.0-SNAPSHOT"
     const val isSnapshot = false
 }
