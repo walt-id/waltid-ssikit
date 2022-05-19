addb1c1273c24cadccc38bcb6ce04a1c8d42fb20
Kevin Burgmann <kevin@walt.id>
2022-05-19 22:56:14 +0200

    Reformat RegoValidator

--- src/main/kotlin/id/walt/auditor/RegoValidator.kt
+++ src/main/kotlin/id/walt/auditor/RegoValidator.kt
@@ -4,7 +4,6 @@ import com.beust.klaxon.JsonArray
 import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Klaxon
 import io.ktor.client.*
-import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
@@ -15,35 +14,37 @@ import java.io.StringReader
 import java.net.URL
 
 object RegoValidator {
-  val client = HttpClient(CIO) {
-    install(ContentNegotiation) {
-      json()
-    }
-  }
-
-  fun validate(input: Map<String, Any?>, data: Map<String, Any?>, regoUrl: URL): Boolean {
-    val rego = runBlocking {
-      client.get(regoUrl).bodyAsText()
-    }
-    return validate(input, data, rego)
-  }
-
-  fun validate(input: Map<String, Any?>, data: Map<String, Any?>, rego: String): Boolean {
-    val validationResultText = runBlocking {
-      client.post("https://play.openpolicyagent.org/v1/data") {
-        setBody(
-          JsonObject(mapOf(
-          "data" to data,
-          "input" to input,
-          "rego_modules" to mapOf("policy.rego" to rego),
-          "strict" to true
-        )).toJsonString().also {
-            println("rego payload: $it")
+    val client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json()
         }
-        )
-      }.bodyAsText()
     }
-    val validationResult = Klaxon().parseJsonObject(StringReader(validationResultText))
-    return (((((validationResult["result"] as JsonArray<*>)[0] as JsonObject)["expressions"] as JsonArray<*>)[0] as JsonObject)["value"] as JsonObject)["allow"] as Boolean
-  }
+
+    fun validate(input: Map<String, Any?>, data: Map<String, Any?>, regoUrl: URL): Boolean {
+        val rego = runBlocking {
+            client.get(regoUrl).bodyAsText()
+        }
+        return validate(input, data, rego)
+    }
+
+    fun validate(input: Map<String, Any?>, data: Map<String, Any?>, rego: String): Boolean {
+        val validationResultText = runBlocking {
+            client.post("https://play.openpolicyagent.org/v1/data") {
+                setBody(
+                    JsonObject(
+                        mapOf(
+                            "data" to data,
+                            "input" to input,
+                            "rego_modules" to mapOf("policy.rego" to rego),
+                            "strict" to true
+                        )
+                    ).toJsonString().also {
+                        println("rego payload: $it")
+                    }
+                )
+            }.bodyAsText()
+        }
+        val validationResult = Klaxon().parseJsonObject(StringReader(validationResultText))
+        return (((((validationResult["result"] as JsonArray<*>)[0] as JsonObject)["expressions"] as JsonArray<*>)[0] as JsonObject)["value"] as JsonObject)["allow"] as Boolean
+    }
 }
