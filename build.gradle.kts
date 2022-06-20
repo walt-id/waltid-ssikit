cd54eda0f916f1b30c0fa36c96b2b5277293033c
Philipp Potisk <phil@walt.id>
2022-06-20 16:52:38 +0200

    Upgraded vclib

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     implementation("com.google.guava:guava:31.1-jre")
 
     // VC
-    implementation("id.walt:waltid-ssikit-vclib:1.21.0")
+    implementation("id.walt:waltid-ssikit-vclib:1.22.0")
 
     // JSON
     implementation("org.json:json:20220320")
