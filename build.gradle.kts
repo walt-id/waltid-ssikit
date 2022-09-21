d707c4768314c5e4f88cd31eefcf2e2c21b31978
Kevin Burgmann <kevin@walt.id>
2022-09-21 02:38:23 +0200

    Update vclib from 1.22.0 to 1.23.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     implementation("com.google.guava:guava:31.1-jre")
 
     // VC
-    implementation("id.walt:waltid-ssikit-vclib:1.22.0")
+    implementation("id.walt:waltid-ssikit-vclib:1.23.0")
 
     // JSON
     implementation("org.json:json:20220320")
