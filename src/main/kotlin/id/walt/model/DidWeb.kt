5a2d1eb711712d3c968903567a727d19868a910e
Kevin Burgmann <kevin@walt.id>
2022-09-27 15:08:26 +0200

    Reformat DidWeb

--- src/main/kotlin/id/walt/model/DidWeb.kt
+++ src/main/kotlin/id/walt/model/DidWeb.kt
@@ -15,38 +15,52 @@ class DidWeb(
     keyAgreement: List<VerificationMethod>? = null,
     serviceEndpoint: List<ServiceEndpoint>? = null
 ) : Did(
-    context,
-    id,
-    verificationMethod,
-    authentication,
-    assertionMethod,
-    capabilityDelegation,
-    capabilityInvocation,
-    keyAgreement,
-    serviceEndpoint
+    context = context,
+    id = id,
+    verificationMethod = verificationMethod,
+    authentication = authentication,
+    assertionMethod = assertionMethod,
+    capabilityDelegation = capabilityDelegation,
+    capabilityInvocation = capabilityInvocation,
+    keyAgreement = keyAgreement,
+    serviceEndpoint = serviceEndpoint
 ) {
-    constructor(context: String,
-                id: String,
-                verificationMethod: List<VerificationMethod>? = null,
-                authentication: List<VerificationMethod>? = null,
-                assertionMethod: List<VerificationMethod>? = null,
-                capabilityDelegation: List<VerificationMethod>? = null,
-                capabilityInvocation: List<VerificationMethod>? = null,
-                keyAgreement: List<VerificationMethod>? = null,
-                serviceEndpoint: List<ServiceEndpoint>? = null) : this(listOf(context), id, verificationMethod, authentication, assertionMethod, capabilityDelegation, capabilityInvocation, keyAgreement, serviceEndpoint)
+    constructor(
+        context: String,
+        id: String,
+        verificationMethod: List<VerificationMethod>? = null,
+        authentication: List<VerificationMethod>? = null,
+        assertionMethod: List<VerificationMethod>? = null,
+        capabilityDelegation: List<VerificationMethod>? = null,
+        capabilityInvocation: List<VerificationMethod>? = null,
+        keyAgreement: List<VerificationMethod>? = null,
+        serviceEndpoint: List<ServiceEndpoint>? = null
+    ) : this(
+        context = listOf(context),
+        id = id,
+        verificationMethod = verificationMethod,
+        authentication = authentication,
+        assertionMethod = assertionMethod,
+        capabilityDelegation = capabilityDelegation,
+        capabilityInvocation = capabilityInvocation,
+        keyAgreement = keyAgreement,
+        serviceEndpoint = serviceEndpoint
+    )
 
     companion object {
-      fun getDomain(didUrl: DidUrl) =
-        didUrl.identifier.substringBefore(":").let { URLDecoder.decode(it, StandardCharsets.UTF_8) }
-      fun getPath(didUrl: DidUrl) =
-        didUrl.identifier.substringAfter(":", "").split(":").map { part -> URLDecoder.decode(part, StandardCharsets.UTF_8) }.joinToString("/")
-      fun getDidDocUri(didUrl: DidUrl): URI {
-        val path = getPath(didUrl)
-        if (path.isEmpty()) {
-          return URI.create("https://${getDomain(didUrl)}/.well-known/did.json")
-        } else {
-          return URI.create("https://${getDomain(didUrl)}/${path}/did.json")
-        }
-      }
+        fun getDomain(didUrl: DidUrl): String =
+            didUrl.identifier.substringBefore(":").let { URLDecoder.decode(it, StandardCharsets.UTF_8) }
+
+        fun getPath(didUrl: DidUrl) =
+            didUrl.identifier.substringAfter(":", "").split(":").map { part -> URLDecoder.decode(part, StandardCharsets.UTF_8) }
+                .joinToString("/")
+
+        fun getDidDocUri(didUrl: DidUrl): URI =
+            getPath(didUrl).let { path ->
+                return when {
+                    path.isEmpty() -> URI.create("https://${getDomain(didUrl)}/.well-known/did.json")
+                    else -> URI.create("https://${getDomain(didUrl)}/${path}/did.json")
+                }
+            }
     }
 }
