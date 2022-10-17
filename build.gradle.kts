c727247a5811f19cbc4a186a2fd1d6ebe1d70be5
Kevin Burgmann <kevin@walt.id>
2022-10-17 15:23:25 +0200

    Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,14 +27,15 @@ dependencies {
     api("info.weboftrust:ld-signatures-java:1.0.0")
     api("decentralized-identity:jsonld-common-java:1.1.0")
     implementation("com.goterl:lazysodium-java:5.1.1")
+    implementation("net.java.dev.jna:jna:5.12.1")
     implementation("com.github.multiformats:java-multibase:v1.1.0")
     implementation("com.microsoft.azure:azure-keyvault:1.2.6")
     implementation("com.microsoft.azure:azure-client-authentication:1.7.14")
-    implementation("com.nimbusds:nimbus-jose-jwt:9.25.1")
+    implementation("com.nimbusds:nimbus-jose-jwt:9.25.4")
     implementation("com.nimbusds:oauth2-oidc-sdk:9.43.1")
 
-    implementation("org.bouncycastle:bcprov-jdk15to18:1.71")
-    implementation("org.bouncycastle:bcpkix-jdk15to18:1.71")
+    implementation("org.bouncycastle:bcprov-jdk15to18:1.72")
+    implementation("org.bouncycastle:bcpkix-jdk15to18:1.72")
 
     // Ethereum
     implementation("org.web3j:core:5.0.0")
@@ -46,11 +47,11 @@ dependencies {
     implementation("id.walt:waltid-ssikit-vclib:1.23.4")
 
     // JSON
-    implementation("org.json:json:20220320")
+    implementation("org.json:json:20220924")
     implementation("com.beust:klaxon:5.6")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.4")
-    implementation("io.ktor:ktor-client-jackson:2.1.1")
-    implementation("io.ktor:ktor-client-content-negotiation:2.1.1")
+    implementation("io.ktor:ktor-client-jackson:2.1.2")
+    implementation("io.ktor:ktor-client-content-negotiation:2.1.2")
     implementation("com.jayway.jsonpath:json-path:2.7.0")
 
     //implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
@@ -65,47 +66,47 @@ dependencies {
 
     // Misc
     implementation("commons-io:commons-io:2.11.0")
-    implementation("io.minio:minio:8.4.4")
+    implementation("io.minio:minio:8.4.5")
 
     // HTTP
-    implementation("io.ktor:ktor-client-core:2.1.1")
-    implementation("io.ktor:ktor-client-cio:2.1.1")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.1")
-    implementation("io.ktor:ktor-client-logging:2.1.1")
+    implementation("io.ktor:ktor-client-core:2.1.2")
+    implementation("io.ktor:ktor-client-cio:2.1.2")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.2")
+    implementation("io.ktor:ktor-client-logging:2.1.2")
     implementation("io.github.rybalkinsd:kohttp:0.12.0")
 
     // REST
-    implementation("io.javalin:javalin:4.6.4")
-    implementation("io.javalin:javalin-openapi:4.6.4")
+    implementation("io.javalin:javalin:4.6.6")
+    implementation("io.javalin:javalin-openapi:4.6.6")
     // implementation("io.javalin:javalin-test-tools:4.5.0")
 
     // Logging
-    implementation("org.slf4j:slf4j-api:2.0.1")
-    implementation("org.slf4j:slf4j-simple:2.0.1")
+    implementation("org.slf4j:slf4j-api:2.0.3")
+    implementation("org.slf4j:slf4j-simple:2.0.3")
 
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.0")
 
     // Config
-    implementation("com.sksamuel.hoplite:hoplite-core:2.6.3")
-    implementation("com.sksamuel.hoplite:hoplite-yaml:2.6.3")
-    implementation("com.sksamuel.hoplite:hoplite-hikaricp:2.6.3")
+    implementation("com.sksamuel.hoplite:hoplite-core:2.6.4")
+    implementation("com.sksamuel.hoplite:hoplite-yaml:2.6.4")
+    implementation("com.sksamuel.hoplite:hoplite-hikaricp:2.6.4")
 
     // Service-Matrix
     implementation("id.walt.servicematrix:WaltID-ServiceMatrix:1.1.2")
 
     // Kotlin
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.10")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.20")
 
     // JNR-FFI
     implementation("com.github.jnr:jnr-ffi:2.2.12")
 
     // Testing
     //testImplementation(kotlin("test-junit"))
-    testImplementation("io.mockk:mockk:1.12.8")
+    testImplementation("io.mockk:mockk:1.13.2")
 
-    testImplementation("io.kotest:kotest-runner-junit5:5.4.2")
-    testImplementation("io.kotest:kotest-assertions-core:5.4.2")
-    testImplementation("io.kotest:kotest-assertions-json:5.4.2")
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.5.0")
+    testImplementation("io.kotest:kotest-assertions-json:5.5.0")
 }
 
 tasks.withType<Test> {
