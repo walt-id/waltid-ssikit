a5a3be8d604632a7820fdd92eb2d5c1e4681dffc
severinstampler <severin@walt.id>
2022-11-07 13:57:59 +0100

    update vclib

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation("com.google.guava:guava:31.1-jre")
 
     // VC
-    implementation("id.walt:waltid-ssikit-vclib:1.24.0")
+    implementation("id.walt:waltid-ssikit-vclib:1.24.1")
 
     // JSON
     implementation("org.json:json:20220924")
