144c3f8beadd865f7d97853836d69be8df4ef4b6
Thomas Diesler <tdiesler@redhat.com>
2023-02-22 17:53:04 +0100

    [#213] Allow clients to define the initial set of verification policies

--- src/main/kotlin/id/walt/auditor/PolicyRegistry.kt
+++ src/main/kotlin/id/walt/auditor/PolicyRegistry.kt
@@ -1,15 +1,9 @@
 package id.walt.auditor
 
 import com.beust.klaxon.JsonObject
-import com.beust.klaxon.Klaxon
 import id.walt.auditor.dynamic.DynamicPolicy
 import id.walt.auditor.dynamic.DynamicPolicyArg
 import id.walt.common.deepMerge
-import id.walt.common.resolveContent
-import id.walt.model.dif.PresentationDefinition
-import id.walt.services.context.ContextManager
-import id.walt.services.hkvstore.HKVKey
-import java.io.StringReader
 import kotlin.reflect.KClass
 import kotlin.reflect.full.createInstance
 import kotlin.reflect.full.primaryConstructor
@@ -61,149 +55,37 @@ class DynamicPolicyFactory(
 }
 
 object PolicyRegistry {
-    const val SAVED_POLICY_ROOT_KEY = "policies"
-    private var _policies: LinkedHashMap<String, PolicyFactory<*, *>>? = null
-    private val policies: LinkedHashMap<String, PolicyFactory<*, *>>
-        get() {
-            if (_policies == null) {
-                initPolicies()
-            }
-            return _policies!!
-        }
-    val defaultPolicyId: String = SignaturePolicy::class.simpleName!!
 
-    fun <P : ParameterizedVerificationPolicy<A>, A : Any> register(
-        policy: KClass<P>,
-        argType: KClass<A>,
-        description: String? = null
-    ) = policies.put(policy.simpleName!!, PolicyFactory(policy, argType, policy.simpleName!!, description))
+    private val delegate = PolicyRegistryService.getService()
+
+    val defaultPolicyId: String = delegate.defaultPolicyId
+
+    fun <P : ParameterizedVerificationPolicy<A>, A : Any> register(policy: KClass<P>, argType: KClass<A>, description: String? = null) =
+        delegate.register(policy, argType, description)
 
     fun <P : SimpleVerificationPolicy> register(policy: KClass<P>, description: String? = null) =
-        policies.put(policy.simpleName!!, PolicyFactory<P, Unit>(policy, null, policy.simpleName!!, description))
+        delegate.register(policy, description)
 
-    fun registerSavedPolicy(name: String, dynamicPolicyArg: DynamicPolicyArg, immutable: Boolean = false) = policies.put(
-        name,
-        DynamicPolicyFactory(dynamicPolicyArg, immutable, name = name, description = dynamicPolicyArg.description)
-    )
+    fun <A : Any> getPolicy(id: String, argument: A? = null) =
+        delegate.getPolicy(id, argument)
 
-    fun <A : Any> getPolicy(id: String, argument: A? = null) = policies[id]!!.create(argument)
-    fun getPolicy(id: String) = getPolicy(id, null)
-    fun contains(id: String) = policies.containsKey(id)
-    fun listPolicies() = policies.keys
-    fun listPolicyInfo() = policies.values.map { p ->
-        VerificationPolicyMetadata(
-            p.name,
-            p.description,
-            p.requiredArgumentType,
-            isMutable(p.name)
-        )
-    }
+    fun getPolicy(id: String) = delegate.getPolicy(id)
+    fun contains(id: String) = delegate.contains(id)
+    fun listPolicies() = delegate.listPolicies()
+    fun listPolicyInfo() = delegate.listPolicyInfo()
 
-    fun getPolicyWithJsonArg(id: String, argumentJson: JsonObject?): VerificationPolicy {
-        val policyFactory = policies[id] ?: throw IllegalArgumentException("No policy exists with id: $id")
-        val argument =
-            policyFactory.argType?.let {
-                argumentJson?.let {
-                    if (policyFactory.argType == JsonObject::class) {
-                        argumentJson
-                    } else {
-                        Klaxon().fromJsonObject(
-                            argumentJson,
-                            policyFactory.argType.java,
-                            policyFactory.argType
-                        )
-                    }
-                }
-            }
+    fun getPolicyWithJsonArg(id: String, argumentJson: JsonObject?): VerificationPolicy =
+        delegate.getPolicyWithJsonArg(id, argumentJson)
 
-        return policyFactory.create(argument)
-    }
+    fun getPolicyWithJsonArg(id: String, argumentJson: String?): VerificationPolicy  =
+        delegate.getPolicyWithJsonArg(id, argumentJson)
 
-    fun getPolicyWithJsonArg(id: String, argumentJson: String?): VerificationPolicy {
-        return getPolicyWithJsonArg(id, argumentJson?.let { Klaxon().parseJsonObject(StringReader(it)) })
-    }
+    fun isMutable(name: String): Boolean =
+        delegate.isMutable(name)
 
-    fun isMutable(name: String): Boolean {
-        val polF = policies[name] ?: return false
-        return polF is DynamicPolicyFactory && !polF.immutable
-    }
+    fun createSavedPolicy(name: String, dynPolArg: DynamicPolicyArg, override: Boolean, download: Boolean): Boolean =
+        delegate.createSavedPolicy(name, dynPolArg, override, download)
 
-    fun createSavedPolicy(name: String, dynPolArg: DynamicPolicyArg, override: Boolean, download: Boolean): Boolean {
-        if (!contains(name) || (isMutable(name) && override)) {
-            val policyContent = when (download) {
-                true -> resolveContent(dynPolArg.policy)
-                false -> dynPolArg.policy
-            }
-            val dynPolArgMod = DynamicPolicyArg(
-                name,
-                dynPolArg.description,
-                dynPolArg.input,
-                policyContent,
-                dynPolArg.dataPath,
-                dynPolArg.policyQuery,
-                dynPolArg.policyEngine,
-                dynPolArg.applyToVC,
-                dynPolArg.applyToVP
-            )
-            ContextManager.hkvStore.put(HKVKey(SAVED_POLICY_ROOT_KEY, name), Klaxon().toJsonString(dynPolArgMod))
-            registerSavedPolicy(name, dynPolArgMod)
-            return true
-        }
-        return false
-    }
-
-    fun deleteSavedPolicy(name: String): Boolean {
-        if (isMutable(name)) {
-            ContextManager.hkvStore.delete(HKVKey(SAVED_POLICY_ROOT_KEY, name))
-            policies.remove(name)
-            return true
-        }
-        return false
-    }
-
-    private fun initSavedPolicies() {
-        ContextManager.hkvStore.listChildKeys(HKVKey(SAVED_POLICY_ROOT_KEY)).forEach {
-            registerSavedPolicy(it.name, Klaxon().parse(ContextManager.hkvStore.getAsString(it)!!)!!)
-        }
-    }
-
-    private fun initPolicies() {
-        _policies = linkedMapOf()
-        register(SignaturePolicy::class, "Verify by signature")
-        //register(JsonSchemaPolicy::class, "Verify by JSON schema")
-        register(TrustedSchemaRegistryPolicy::class, "Verify by EBSI Trusted Schema Registry")
-        register(TrustedIssuerDidPolicy::class, "Verify by trusted issuer did")
-        register(TrustedIssuerRegistryPolicy::class, "Verify by trusted EBSI Trusted Issuer Registry record")
-        register(TrustedSubjectDidPolicy::class, "Verify by trusted subject did")
-        register(IssuedDateBeforePolicy::class, "Verify by issuance date")
-        register(ValidFromBeforePolicy::class, "Verify by valid from")
-        register(ExpirationDateAfterPolicy::class, "Verify by expiration date")
-        //register(GaiaxTrustedPolicy::class, "Verify Gaiax trusted fields")
-        register(GaiaxSDPolicy::class, "Verify Gaiax SD fields")
-        register(ChallengePolicy::class, ChallengePolicyArg::class, "Verify challenge")
-        register(
-            PresentationDefinitionPolicy::class,
-            PresentationDefinition::class,
-            "Verify that verifiable presentation complies with presentation definition"
-        )
-        register(CredentialStatusPolicy::class, "Verify by credential status")
-        register(DynamicPolicy::class, DynamicPolicyArg::class, "Verify credential by rego policy")
-
-        // predefined, hardcoded rego policy specializations
-        // VerifiableMandate policy as specialized rego policy
-        registerSavedPolicy(
-            "VerifiableMandatePolicy", DynamicPolicyArg(
-                "VerifiableMandatePolicy", "Predefined policy for verifiable mandates",
-                JsonObject(), "$.credentialSubject.policySchemaURI",
-                "$.credentialSubject.holder", "data.system.main"
-            ),
-            immutable = true
-        )
-
-        // other saved (Rego) policies
-        initSavedPolicies()
-
-        //RegoPolicy(RegoPolicyArg(mapOf(), "")).argument.input
-    }
-
+    fun deleteSavedPolicy(name: String): Boolean =
+        delegate.deleteSavedPolicy(name)
 }
