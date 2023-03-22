ebd5ec4b2d1aef50da9bfa6df3e47c774975fe36
Kevin Burgmann <kevin@walt.id>
2023-03-22 11:27:49 +0100

    Supply exception to failure for JsonSchemaPolicy

--- src/main/kotlin/id/walt/auditor/VerificationPolicy.kt
+++ src/main/kotlin/id/walt/auditor/VerificationPolicy.kt
@@ -105,14 +105,15 @@ class SignaturePolicy : SimpleVerificationPolicy() {
  */
 data class JsonSchemaPolicyArg(val schema: String)
 
-class JsonSchemaPolicy(schemaPolicyArg: JsonSchemaPolicyArg?) : OptionalParameterizedVerificationPolicy<JsonSchemaPolicyArg>(schemaPolicyArg) {
+class JsonSchemaPolicy(schemaPolicyArg: JsonSchemaPolicyArg?) :
+    OptionalParameterizedVerificationPolicy<JsonSchemaPolicyArg>(schemaPolicyArg) {
     constructor() : this(null)
 
     override val description: String = "Verify by JSON schema"
     override fun doVerify(vc: VerifiableCredential): VerificationPolicyResult {
         return (argument?.schema ?: vc.credentialSchema?.id)?.let {
             SchemaValidatorFactory.get(it).validate(vc.toJson())
-        } ?: VerificationPolicyResult.failure()
+        } ?: VerificationPolicyResult.failure(IllegalArgumentException("No \"argument.schema\" or \"credentialSchema.id\" supplied."))
     }
 
     override val applyToVP: Boolean
