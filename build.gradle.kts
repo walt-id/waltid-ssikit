8a6fe49303fb0527040089d1c19199a98127fc9f
mikeplotean <mike@walt.id>
2023-03-22 13:51:09 +0200

    refactor: did service
    # Conflicts:
    #	src/main/kotlin/id/walt/services/did/DidService.kt
    
    # Conflicts:
    #	src/main/kotlin/id/walt/services/did/DidService.kt

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     implementation("io.ktor:ktor-client-content-negotiation:2.2.4")
     implementation("io.ktor:ktor-client-core-jvm:2.2.4")
     implementation("io.ktor:ktor-client-cio-jvm:2.2.4")
+    implementation("io.ktor:ktor-client-auth:2.0.3")
     implementation("io.ktor:ktor-client-okhttp:2.2.4")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.2.4")
     implementation("io.ktor:ktor-client-logging-jvm:2.2.4")
--- src/main/kotlin/id/walt/services/did/resolvers/DidIotaResolver.kt
+++ src/main/kotlin/id/walt/services/did/resolvers/DidIotaResolver.kt
@@ -1,4 +1,14 @@
 package id.walt.services.did.resolvers
 
-class DidIotaResolver {
+import id.walt.model.Did
+import id.walt.model.DidUrl
+import id.walt.model.did.DidIota
+import id.walt.services.ecosystems.iota.IotaWrapper
+
+class DidIotaResolver(private val iotaWrapper: IotaWrapper) : DidResolverBase<DidIota>() {
+    override fun resolve(didUrl: DidUrl): Did = iotaWrapper.resolve_did(didUrl.did).takeIf { it.address() != 0L }?.let {
+        val doc = it.getString(0)
+        iotaWrapper.free_str(it)
+        Did.decode(doc)
+    } ?: throw IllegalArgumentException("Could not resolve $didUrl")
 }
