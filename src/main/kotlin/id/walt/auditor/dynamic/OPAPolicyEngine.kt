6ce9a1b2a6c52f900ca0a54bcd3497271183ebbd
mikeplotean <mike@walt.id>
2023-03-30 18:34:07 +0300

    refactor: verification-policy-result implement operation-result-pattern

--- src/main/kotlin/id/walt/auditor/dynamic/OPAPolicyEngine.kt
+++ src/main/kotlin/id/walt/auditor/dynamic/OPAPolicyEngine.kt
@@ -1,14 +1,9 @@
 package id.walt.auditor.dynamic
 
-import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Klaxon
-import com.beust.klaxon.Parser
 import id.walt.auditor.VerificationPolicyResult
 import id.walt.common.resolveContentToFile
-import id.walt.credentials.w3c.JsonConverter
-import kotlinx.serialization.json.buildJsonObject
 import mu.KotlinLogging
-import java.io.File
 
 object OPAPolicyEngine : PolicyEngine {
     private val log = KotlinLogging.logger {}
@@ -37,7 +32,8 @@ object OPAPolicyEngine : PolicyEngine {
             val output = p.inputStream.reader().use { it.readText() }
             p.waitFor()
             log.debug("rego eval output: {}", output)
-            return VerificationPolicyResult(Klaxon().parseArray<Boolean>(output)?.all { it } ?: false)
+            return (Klaxon().parseArray<Boolean>(output)?.all { it } ?: false).takeIf { it }
+                ?.let { VerificationPolicyResult.success() } ?: VerificationPolicyResult.failure()
         } finally {
             if (regoFile.exists() && regoFile.name.startsWith(TEMP_PREFIX)) {
                 regoFile.delete()
