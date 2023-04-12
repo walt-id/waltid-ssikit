ef3f15dc409f3599f522417ecae4acd710aa6c5c
mikeplotean <mike@walt.id>
2023-04-12 19:55:29 +0300

    status list 2021 entry: check revocation

--- src/main/kotlin/id/walt/common/CommonUtils.kt
+++ src/main/kotlin/id/walt/common/CommonUtils.kt
@@ -4,7 +4,10 @@ import id.walt.services.WaltIdServices.httpNoAuth
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlinx.coroutines.runBlocking
+import java.io.ByteArrayOutputStream
 import java.io.File
+import java.util.*
+import java.util.zip.*
 
 fun resolveContent(fileUrlContent: String): String {
     val file = File(fileUrlContent)
@@ -34,4 +37,45 @@ fun resolveContentToFile(fileUrlContent: String, tempPrefix: String = "TEMP", te
         }
     }
     return fileCheck
+}
+
+fun compressGzip(data: ByteArray): ByteArray {
+    val result = ByteArrayOutputStream()
+    GZIPOutputStream(result).use {
+        it.write(data)
+    }
+    return result.toByteArray()
+}
+
+fun uncompressGzip(data: ByteArray, idx: ULong? = null) =
+    GZIPInputStream(data.inputStream()).bufferedReader().use {
+        idx?.let { index ->
+            var int = it.read()
+            var count = 0U
+            var char = int.toChar()
+            while (int != -1 && count++ <= index) {
+                char = int.toChar()
+                int = it.read()
+            }
+            char
+        }?.let {
+            val array = CharArray(1)
+            array[0] = it
+            array
+        } ?: it.readText().toCharArray()
+    }
+
+fun main(){
+    val bitstring = (0..7).map {
+        (it % 2).digitToChar()
+    }.joinToString("")
+    println("bitstring: $bitstring")
+    val compressed = compressGzip(bitstring.toByteArray())
+    println("compressed: $compressed")
+    val encoded = Base64.getEncoder().encode(compressed)
+    println("encoded: ${encoded.decodeToString()}")
+    val decoded = Base64.getDecoder().decode(encoded)
+    println("decoded: ${decoded.decodeToString()}}")
+    val revocationList = uncompressGzip(decoded, 94564U)
+    println("uncompressed: ${String(revocationList)}")
 }
