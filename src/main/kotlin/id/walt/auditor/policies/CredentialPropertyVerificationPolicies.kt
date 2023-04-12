8c2fb5a2df5a499c42dc2335150a973775bbd8e6
mikeplotean <mike@walt.id>
2023-04-12 19:55:29 +0300

    status list 2021 entry: check revocation

--- src/main/kotlin/id/walt/auditor/policies/CredentialPropertyVerificationPolicies.kt
+++ src/main/kotlin/id/walt/auditor/policies/CredentialPropertyVerificationPolicies.kt
@@ -7,7 +7,11 @@ import id.walt.auditor.SimpleVerificationPolicy
 import id.walt.auditor.VerificationPolicyResult
 import id.walt.credentials.w3c.VerifiableCredential
 import id.walt.credentials.w3c.VerifiablePresentation
+import id.walt.model.credential.status.CredentialStatus
+import id.walt.model.credential.status.SimpleCredentialStatus2022
+import id.walt.model.credential.status.StatusList2021EntryCredentialStatus
 import id.walt.signatory.RevocationClientService
+import id.walt.signatory.revocation.StatusList2021EntryService
 import kotlinx.serialization.Serializable
 import java.text.SimpleDateFormat
 import java.util.*
@@ -48,34 +52,40 @@ class ExpirationDateAfterPolicy : SimpleVerificationPolicy() {
 class CredentialStatusPolicy : SimpleVerificationPolicy() {
 
     @Serializable
-    data class CredentialStatus(
-        val id: String,
-        var type: String
-    )
-
-    @Serializable
     data class CredentialStatusCredential(
         @Json(serializeNull = false) var credentialStatus: CredentialStatus? = null
     )
 
     override val description: String = "Verify by credential status"
-    override fun doVerify(vc: VerifiableCredential): VerificationPolicyResult {
-        val cs = Klaxon().parse<CredentialStatusCredential>(vc.toJson())!!.credentialStatus!!
-
-        fun revocationVerificationPolicy(revoked: Boolean, timeOfRevocation: Long?) =
-            if (!revoked) VerificationPolicyResult.success() else VerificationPolicyResult.failure(IllegalArgumentException("CredentialStatus (type ${cs.type}) was REVOKED at timestamp $timeOfRevocation for id ${cs.id}."))
-
-        return when (cs.type) {
-            "SimpleCredentialStatus2022" -> {
-                val rs = RevocationClientService.getService()
-                val result = rs.checkRevoked(cs.id)
-                revocationVerificationPolicy(result.isRevoked, result.timeOfRevocation)
+    override fun doVerify(vc: VerifiableCredential): VerificationPolicyResult = runCatching {
+        Klaxon().parse<CredentialStatusCredential>(vc.toJson())!!.credentialStatus!!.let {
+            when (it) {
+                is SimpleCredentialStatus2022 -> checkSimpleRevocation(it)
+                is StatusList2021EntryCredentialStatus -> checkStatusListRevocation(it)
             }
-            "StatusList2021Credential" -> VerificationPolicyResult.success()//TODO: implement
-            "CredentialStatusList2020" -> VerificationPolicyResult.success()//TODO: implement
-            else -> VerificationPolicyResult.failure(UnsupportedOperationException("CredentialStatus type \"${cs.type}\" is not yet supported."))
         }
+    }.getOrElse {
+        VerificationPolicyResult.failure(it)
     }
+
+    private fun checkSimpleRevocation(cs: CredentialStatus) = let {
+        fun revocationVerificationPolicy(revoked: Boolean, timeOfRevocation: Long?) =
+            if (!revoked) VerificationPolicyResult.success()
+            else VerificationPolicyResult.failure(
+                IllegalArgumentException("CredentialStatus (type ${cs.type}) was REVOKED at timestamp $timeOfRevocation for id ${cs.id}.")
+            )
+
+        val rs = RevocationClientService.getService()
+        val result = rs.checkRevoked(cs.id)
+        revocationVerificationPolicy(result.isRevoked, result.timeOfRevocation)
+    }
+
+    private fun checkStatusListRevocation(cs: StatusList2021EntryCredentialStatus) =
+        StatusList2021EntryService.checkRevoked(cs).let {
+            it.takeIf { !it }?.let {
+                VerificationPolicyResult.success()
+            } ?: VerificationPolicyResult.failure(Throwable("CredentialStatus ${cs.type} was REVOKED for id ${cs.id}"))
+        }
 }
 
 data class ChallengePolicyArg(val challenges: Set<String>, val applyToVC: Boolean = true, val applyToVP: Boolean = true)
---
+++ src/main/kotlin/id/walt/signatory/revocation/RevocationService.kt
@@ -0,0 +1,52 @@
+package id.walt.signatory.revocation
+
+import com.beust.klaxon.Json
+import id.walt.model.credential.status.StatusList2021EntryCredentialStatus
+import kotlinx.serialization.Serializable
+
+interface RevocationService {
+    fun checkRevocation(parameter: RevocationParameter): RevocationResult
+    fun getRevocation(): RevocationData
+    fun clearAll()
+    fun setRevocation(parameter: RevocationParameter)
+}
+
+data class RevocationList(val revokedList: List<RevocationResult>)
+
+/*
+Revocation results
+ */
+@Serializable
+abstract class RevocationResult {
+    abstract val isRevoked: Boolean
+}
+
+@Serializable
+data class TokenRevocationResult(
+    val token: String,
+    override val isRevoked: Boolean,
+    @Json(serializeNull = false)
+    val timeOfRevocation: Long? = null
+) : RevocationResult()
+
+@Serializable
+data class StatusListRevocationResult(
+    override val isRevoked: Boolean
+) : RevocationResult()
+
+/*
+Revocation parameters
+ */
+interface RevocationParameter
+data class TokenRevocationParameter(
+    val token: String,
+) : RevocationParameter
+
+data class StatusListRevocationParameter(
+    val credentialStatus: StatusList2021EntryCredentialStatus,
+) : RevocationParameter
+
+/*
+Revocation data
+ */
+interface RevocationData
