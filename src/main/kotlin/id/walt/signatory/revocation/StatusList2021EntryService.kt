ce9af5aa19f057e0ee5233d6ffdcd89b627fd46c
mikeplotean <mike@walt.id>
2023-04-21 19:59:17 +0300

    revocation config
    # Conflicts:
    #	src/main/kotlin/id/walt/common/CommonUtils.kt

--- src/main/kotlin/id/walt/signatory/revocation/StatusList2021EntryService.kt
+++ src/main/kotlin/id/walt/signatory/revocation/StatusList2021EntryService.kt
@@ -1,15 +1,17 @@
 package id.walt.signatory.revocation
 
 import com.beust.klaxon.Json
-import id.walt.common.resolveContent
-import id.walt.common.uncompressGzip
+import id.walt.common.*
 import id.walt.credentials.w3c.VerifiableCredential
 import id.walt.credentials.w3c.toVerifiableCredential
 import id.walt.crypto.decBase64
 import id.walt.model.credential.status.StatusList2021EntryCredentialStatus
 import kotlinx.serialization.Serializable
+import java.util.*
 
 object StatusList2021EntryService {
+
+    private val bitSet = BitSet(16*1024*8)
 
     fun checkRevoked(credentialStatus: StatusList2021EntryCredentialStatus): Boolean = let {
         val credentialSubject = extractStatusListCredentialSubject(credentialStatus.statusListCredential) ?: throw IllegalArgumentException("Couldn't parse credential subject")
@@ -17,6 +19,16 @@ object StatusList2021EntryService {
         if(!verifyStatusPurpose(credentialStatus.statusPurpose, credentialSubject.statusPurpose)) throw IllegalArgumentException("Status purposes don't match")
 
         verifyBitStringStatus(credentialIndex, credentialSubject.encodedList)
+    }
+
+    fun setRevocation(config: RevocationConfig) {
+        val id = config as StatusListRevocationConfig
+        // lookup credential-index based on id
+        val idx = 0//TODO: look it up
+        // set the respective bit
+        bitSet.set(idx)
+        val bitString = createEncodedBitString(bitSet)
+        // generate status-list credential and store it
     }
 
     private fun extractStatusListCredentialSubject(statusCredential: VerifiableCredential) =
