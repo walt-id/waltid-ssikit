b67a946011a4ce5c8b31a41a3af14790691a9c36
severinstampler <severin@walt.id>
2023-05-30 12:20:57 +0200

    test for issue 52 by majd

--- src/test/kotlin/id/walt/services/did/DidWebTest.kt
+++ src/test/kotlin/id/walt/services/did/DidWebTest.kt
@@ -8,12 +8,16 @@ import id.walt.model.DidMethod
 import id.walt.model.DidUrl
 import id.walt.model.did.DidWeb
 import id.walt.servicematrix.ServiceMatrix
+import id.walt.services.jwt.JwtService
 import id.walt.services.key.KeyService
 import id.walt.test.RESOURCES_PATH
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.core.test.TestCase
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkObject
 
 
 class DidWebTest : StringSpec({
@@ -92,6 +96,42 @@ class DidWebTest : StringSpec({
         DidWeb.getDomain(didUrl) shouldBe "sub-domain.top-level-domain.com"
     }
 
+    "test issue 52 from MajdT51" {
+        val didDoc = "{\n" +
+                "    \"assertionMethod\" : [\n" +
+                "        \"did:web:3740-2003-c1-1721-bd31-a97a-f8d-ea6b-ae95.ngrok-free.app:alice505506#e84ef4c4a60e4b1b8a1dcdbab0044cca\"\n" +
+                "    ],\n" +
+                "    \"authentication\" : [\n" +
+                "        \"did:web:3740-2003-c1-1721-bd31-a97a-f8d-ea6b-ae95.ngrok-free.app:alice505506#e84ef4c4a60e4b1b8a1dcdbab0044cca\"\n" +
+                "    ],\n" +
+                "    \"@context\" : \"https://www.w3.org/ns/did/v1\",\n" +
+                "    \"id\" : \"did:web:3740-2003-c1-1721-bd31-a97a-f8d-ea6b-ae95.ngrok-free.app:alice505506\",\n" +
+                "    \"verificationMethod\" : [\n" +
+                "        {\n" +
+                "            \"controller\" : \"did:web:3740-2003-c1-1721-bd31-a97a-f8d-ea6b-ae95.ngrok-free.app:alice505506\",\n" +
+                "            \"id\" : \"did:web:3740-2003-c1-1721-bd31-a97a-f8d-ea6b-ae95.ngrok-free.app:alice505506#e84ef4c4a60e4b1b8a1dcdbab0044cca\",\n" +
+                "            \"publicKeyJwk\" : {\n" +
+                "                \"alg\" : \"EdDSA\",\n" +
+                "                \"crv\" : \"Ed25519\",\n" +
+                "                \"kid\" : \"e84ef4c4a60e4b1b8a1dcdbab0044cca\",\n" +
+                "                \"kty\" : \"OKP\",\n" +
+                "                \"use\" : \"sig\",\n" +
+                "                \"x\" : \"Yk3kdhlIl-25kOs0cPzKD5J6sYAU1R8kDo5Apylvfa4\"\n" +
+                "            },\n" +
+                "            \"type\" : \"Ed25519VerificationKey2019\"\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}"
+        val didProof = "eyJraWQiOiJkaWQ6d2ViOjM3NDAtMjAwMy1jMS0xNzIxLWJkMzEtYTk3YS1mOGQtZWE2Yi1hZTk1Lm5ncm9rLWZyZWUuYXBwOmFsaWNlNTA1NTA2I2U4NGVmNGM0YTYwZTRiMWI4YTFkY2RiYWIwMDQ0Y2NhIiwidHlwIjoiSldUIiwiYWxnIjoiRWREU0EifQ.eyJpc3MiOiJkaWQ6d2ViOjM3NDAtMjAwMy1jMS0xNzIxLWJkMzEtYTk3YS1mOGQtZWE2Yi1hZTk1Lm5ncm9rLWZyZWUuYXBwOmFsaWNlNTA1NTA2IiwiYXVkIjoiaHR0cHM6Ly9pc3N1ZXIud2FsdC5pZC9pc3N1ZXItYXBpL2RlZmF1bHQvb2lkYyIsImlhdCI6MTY4NTAwNzkzMiwibm9uY2UiOiJiMjQ2OTVlMi1iNzNlLTQ5NzMtOWM1My1mYTA3ZTMyMzNkNzAifQ.B7dpAIpm8P3LPMqmFrCjZSceiN7TYPgEiiMXfbRBCsbDkXYFZO-Bqm-6zSnziQZ6Fi04xoTi0REan_MVwEFrCw"
+
+        val did = Did.decode(didDoc)
+        did shouldNotBe null
+        did!!.id shouldBe "did:web:3740-2003-c1-1721-bd31-a97a-f8d-ea6b-ae95.ngrok-free.app:alice505506"
+        mockkObject(DidService)
+        every { DidService.loadOrResolveAnyDid(did.id) } returns did
+        DidService.importKeys(did.id)
+        JwtService.getService().verify(didProof) shouldBe true
+    }
 }) {
 
     override suspend fun beforeTest(testCase: TestCase) {
