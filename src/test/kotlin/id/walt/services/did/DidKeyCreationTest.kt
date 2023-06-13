a7a904c464a0f219c137bcd252103b67e1cdf543
mikeplotean <mike@walt.id>
2023-06-13 21:30:02 +0300

    jwk_jcs-pub did:key test

--- src/test/kotlin/id/walt/services/did/DidKeyCreationTest.kt
+++ src/test/kotlin/id/walt/services/did/DidKeyCreationTest.kt
@@ -5,19 +5,23 @@ import id.walt.crypto.KeyId
 import id.walt.model.DidMethod
 import id.walt.servicematrix.ServiceMatrix
 import id.walt.services.crypto.CryptoService
+import id.walt.services.key.KeyService
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
 class DidKeyCreationTest : StringSpec({
     ServiceMatrix("service-matrix.properties")
 
     val cryptoService = CryptoService.getService()
+    val keyService = KeyService.getService()
 
-    fun createAndLoadDid(key: KeyId) {
-        val did = DidService.create(DidMethod.key, key.id)
+    fun createAndLoadDid(key: KeyId, options: DidOptions? = null) = let {
+        val did = DidService.create(DidMethod.key, key.id, options)
         println("Created: $did")
 
         val loaded = DidService.load(did)
         println("Loaded: $loaded")
+        loaded
     }
 
     "Create default did:key" {
@@ -47,6 +51,10 @@ class DidKeyCreationTest : StringSpec({
         createAndLoadDid(key)
     }
 
-
+    "Create jwk_jcs-pub did:key" {
+        val jwkPubKey = "{\"kty\":\"EC\",\"crv\":\"P-256\",\"x\":\"ngy44T1vxAT6Di4nr-UaM9K3Tlnz9pkoksDokKFkmNc\",\"y\":\"QCRfOKlSM31GTkb4JHx3nXB4G_jSPMsbdjzlkT_UpPc\"}"
+        val keyId = keyService.importKey(jwkPubKey)
+        val result = createAndLoadDid(keyId, DidKeyCreateOptions(isJwk = true))
+        result.id shouldBe "did:key:z2dmzD81cgPx8Vki7JbuuMmFYrWPgYoytykUZ3eyqht1j9KbsEYvdrjxMjQ4tpnje9BDBTzuNDP3knn6qLZErzd4bJ5go2CChoPjd5GAH3zpFJP5fuwSk66U5Pq6EhF4nKnHzDnznEP8fX99nZGgwbAh1o7Gj1X52Tdhf7U4KTk66xsA5r"
+    }
 })
-
